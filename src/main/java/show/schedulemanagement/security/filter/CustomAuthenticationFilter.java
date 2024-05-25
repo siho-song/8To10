1aaa04b5127d0476f2f8bca6ce13ea382537708f
siho-song <thdtlgh234@naver.com>
2024-05-25 16:32:31 +0900

    [Feat] 시큐리티 필터 구현
    1. authentication 필터
    2. authorization 필터 구현

---
+++ src/main/java/show/schedulemanagement/security/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package show.schedulemanagement.security.filter;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import show.schedulemanagement.security.dto.LoginRequestDto;
+
+@Slf4j
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        setFilterProcessesUrl("/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        log.info("Attempting authentication for URL: {}", request.getRequestURI());
+        UsernamePasswordAuthenticationToken authRequest;
+        try {
+            authRequest = getAuthRequest(request);
+            setDetails(request, authRequest);
+        } catch (Exception e) {
+            log.error("Error reading login request", e);
+            throw new RuntimeException(e);
+        }
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthRequest(HttpServletRequest request) {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        log.debug("LoginRequestDto: email={}, password={}", email, password);
+
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
+}
---
+++ src/main/java/show/schedulemanagement/security/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,149 @@
+package show.schedulemanagement.security.filter;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.security.SignatureException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+import show.schedulemanagement.security.service.MemberDetailsService;
+import show.schedulemanagement.security.utils.TokenUtils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import org.json.JSONObject;
+
+@RequiredArgsConstructor
+@Slf4j
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final MemberDetailsService memberDetailsService;
+    private final TokenUtils tokenUtils;
+
+    private static final List<String> EXCLUDE_URLS = Arrays.asList(
+            "/",
+            "/signup",
+            "/error",
+            "/login",
+            "/static/**",
+            "/js/**",
+            "/css/**",
+            "/templates/**",
+            "/images/**",
+            "/signup/**"
+    );
+
+    private final AntPathMatcher pathMatcher = new AntPathMatcher();
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();
+        log.info("request.getRequestURI() : {}", requestURI);
+
+        if (shouldExclude(requestURI)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Cookie[] cookies = request.getCookies();
+        String token = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("jwt".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        try {
+            if (token != null && !token.isEmpty()) {
+                if (tokenUtils.isValidToken(token)) {
+                    String loginId = tokenUtils.getUserIdFromToken(token);
+                    log.debug("[+] loginId Check: {}", loginId);
+
+                    if (loginId != null && !loginId.isEmpty()) {
+                        UserDetails userDetails = memberDetailsService.loadUserByUsername(loginId);
+                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                        filterChain.doFilter(request, response);
+                    } else {
+                        throw new UsernameNotFoundException("User Not Found");
+                    }
+                } else {
+                    throw new JwtException("Invalid JWT token");
+                }
+            } else {
+                throw new JwtException("JWT token not found");
+            }
+        } catch (Exception e) {
+            handleException(response, e);
+        }
+    }
+
+    private boolean shouldExclude(String uri) {
+        return EXCLUDE_URLS.stream().anyMatch(pattern -> pathMatcher.match(pattern, uri));
+    }
+
+    private void handleException(HttpServletResponse response, Exception e) throws IOException {
+        String logMessage = jsonResponseWrapper(e).getString("message");
+        log.error(logMessage, e);
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        PrintWriter printWriter = response.getWriter();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("error", true);
+        jsonObject.put("message", "로그인 에러");
+
+        printWriter.print(jsonObject);
+        printWriter.flush();
+        printWriter.close();
+    }
+
+    private JSONObject jsonResponseWrapper(Exception e) {
+        String resultMessage = getResultMessage(e);
+
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("status", 401);
+        jsonMap.put("code", "401");
+        jsonMap.put("message", resultMessage);
+        jsonMap.put("reason", e.getMessage());
+        JSONObject jsonObject = new JSONObject(jsonMap);
+        log.error(resultMessage, e);
+        return jsonObject;
+    }
+
+    private String getResultMessage(Exception e) {
+        if (e instanceof ExpiredJwtException) {
+            return "TOKEN Expired";
+        } else if (e instanceof SignatureException) {
+            return "TOKEN SignatureException Login";
+        } else if (e instanceof JwtException) {
+            return "TOKEN Parsing JwtException";
+        } else {
+            return "OTHER TOKEN ERROR";
+        }
+    }
+}
