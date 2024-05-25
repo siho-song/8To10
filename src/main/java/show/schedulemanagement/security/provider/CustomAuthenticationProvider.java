0b0d4e695b3070a24d08ef8973f19cdffe44241a
siho-song <thdtlgh234@naver.com>
2024-05-25 16:33:19 +0900

    [Feat] security provider 구현

---
+++ src/main/java/show/schedulemanagement/security/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package show.schedulemanagement.security.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.security.dto.MemberDetailsDto;
+import show.schedulemanagement.security.service.MemberDetailsService;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final MemberDetailsService memberDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        MemberDetailsDto member = (MemberDetailsDto) memberDetailsService.loadUserByUsername(email);
+        if (member == null || !bCryptPasswordEncoder.matches(password, member.getPassword())) {
+            throw new BadCredentialsException("Invalid email or password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(email, password, member.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
