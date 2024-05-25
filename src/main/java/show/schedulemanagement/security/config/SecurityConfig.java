cf7753fa7363aaa7f46e671a229764f990c31bad
siho-song <thdtlgh234@naver.com>
2024-05-25 16:36:53 +0900

    [Feat] security configuration 구현

---
+++ src/main/java/show/schedulemanagement/security/config/SecurityConfig.java
@@ -0,0 +1,154 @@
+package show.schedulemanagement.security.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import show.schedulemanagement.security.filter.CustomAuthenticationFilter;
+import show.schedulemanagement.security.filter.JwtAuthorizationFilter;
+import show.schedulemanagement.security.handler.CustomAuthFailureHandler;
+import show.schedulemanagement.security.handler.CustomAuthSuccessHandler;
+import show.schedulemanagement.security.provider.CustomAuthenticationProvider;
+import show.schedulemanagement.security.service.MemberDetailsService;
+import show.schedulemanagement.security.utils.TokenUtils;
+
+import java.util.Collections;
+import java.util.function.Supplier;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class SecurityConfig {
+
+    private final TokenUtils tokenUtils;
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            CustomAuthenticationFilter customAuthenticationFilter,
+            JwtAuthorizationFilter jwtAuthorizationFilter
+    ) throws Exception {
+        log.debug("[+] WebSecurityConfig Start !!! ");
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/signup/**", "/", "/error").permitAll()
+                        .requestMatchers("/css/**", "/resources/**", "/static/**", "/images/**", "/js/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthorizationFilter, BasicAuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/")
+                        .successHandler(new SimpleUrlAuthenticationSuccessHandler("/home"))
+                        .permitAll()
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter(
+            AuthenticationManager authenticationManager,
+            CustomAuthSuccessHandler customAuthSuccessHandler,
+            CustomAuthFailureHandler customAuthFailureHandler
+    ) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
+        customAuthenticationFilter.setFilterProcessesUrl("/login");
+        customAuthenticationFilter.setAuthenticationSuccessHandler(customAuthSuccessHandler);
+        customAuthenticationFilter.setAuthenticationFailureHandler(customAuthFailureHandler);
+        customAuthenticationFilter.afterPropertiesSet();
+        return customAuthenticationFilter;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(CustomAuthenticationProvider customAuthenticationProvider) {
+        return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
+    }
+
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider(MemberDetailsService memberDetailsService) {
+        return new CustomAuthenticationProvider(
+                memberDetailsService,
+                bCryptPasswordEncoder()
+        );
+    }
+
+    @Bean
+    public CustomAuthSuccessHandler customAuthSuccessHandler() {
+        return new CustomAuthSuccessHandler(tokenUtils);
+    }
+
+    @Bean
+    public CustomAuthFailureHandler customAuthFailureHandler() {
+        return new CustomAuthFailureHandler();
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter(MemberDetailsService memberDetailsService) {
+        return new JwtAuthorizationFilter(memberDetailsService, tokenUtils);
+    }
+
+    private AuthorizationDecision isAdmin(
+            Supplier<Authentication> authenticationSupplier,
+            RequestAuthorizationContext requestAuthorizationContext
+    ) {
+        return new AuthorizationDecision(
+                authenticationSupplier.get()
+                        .getAuthorities()
+                        .contains(new SimpleGrantedAuthority("ADMIN"))
+        );
+    }
+
+    @Bean
+    public MemberDetailsService memberDetailsService(show.schedulemanagement.service.MemberServiceImpl memberServiceImpl) {
+        return new MemberDetailsService(memberServiceImpl);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
