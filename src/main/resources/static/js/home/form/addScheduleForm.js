fa93573d70345d7c2a62e2be4d65ccc76b86e3ca
Woosang An <dntkd857@soongsil.ac.kr>
2024-05-26 22:28:11 +0900

    [Feat] 고정일정 폼 변경 (2)
    시간 슬롯에서 일정 정보를 하나씩 불러와서 리퀘스트를 날릴 수 있는 형태로 변경

--- src/main/resources/static/js/home/form/addScheduleForm.js
+++ src/main/resources/static/js/home/form/addScheduleForm.js
@@ -1,101 +1,95 @@
 function submitAddScheduleForm(calendar) {
-    // 공통 입력에 대한 처리
+
     const commonProps = initCommonProperties();
-    const { type, title, startDate, endDate, commonDescription } = commonProps;
-    //type, title, startDate, endDate, description 초기화
+    const { type, title, startDate, endDate, description } = commonProps;
 
     let event = {
         title: title,
-        commonDescription: commonDescription,
-        type: type // 이벤트 타입을 추가하여 구분
+        description: description,
+        type: type
     };
 
-    if (type === 'variable') { //변동 일정
-        const variableProperties = initFixAndVariableProperties(startDate, endDate);
-        const { startDateTime, endDateTime } = variableProperties;
+    if (type === 'fixed') {
 
-        event.start = startDateTime;
-        event.end = endDateTime;
+        event = timeslots.map(slot => {
+            return {
+                title: title,
+                rrule: {
+                    freq: slot.frequency,
+                    interval: 1,
+                    byweekday: slot.days,
+                    dtstart:`${startDate}T${slot.startTime}`,
+                    until: endDate
+                },
+                duration: slot.duration,
+                description:description,
+                color:"#3788d8"
+            }
+        });
 
-        // 서버로 이벤트 객체 전송
-        fetch('http://localhost:8080/schedule/variable/add', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(event)
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-                calendar.addEvent(event);
-                addToDoItem(event); // 일정 추가 시 To-Do 리스트에 반영
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+        const events = timeslots.map(slot => {
+            return {
+                startTime: slot.startTime,
+                duration: `${slot.duration}:00`,
+                frequency: slot.frequency,
+                days: slot.days
+            };
+        });
 
-        console.log(event)
+        const fixedEvents = {
+            title:title,
+            commonDescription:description,
+            startDate:startDate,
+            endDate:endDate,
+            events:events
+        }
 
-    }
-
-
-    //고정 일정
-    else if (type === 'fixed') { //고정 일정
-        const fixedProperties = initFixAndVariableProperties(startDate, endDate);
-        const { startDateTime, endDateTime } = fixedProperties;
-
-        const frequency = document.getElementById('schedule-frequency').value;
-        const weekdays = Array.from(document.querySelectorAll('input[name="weekdays"]:checked')).map(checkbox => checkbox.value);
-
-        event.start = startDateTime;
-        event.end = endDateTime;
-        event.rrule = {
-            freq: frequency.toUpperCase(),
-            interval: 1,
-            byweekday: weekdays,
-            dtstart: startDateTime,
-            until: endDateTime
-        };
-        event.duration = '01:00'; // 고정 일정의 지속 시간 설정
+        console.log(fixedEvents);
 
 
 
-        calendar.addEvent(event);
-        addToDoItem(event); // 일정 추가 시 To-Do 리스트에 반영
-    }
+        event.forEach(e => calendar.addEvent(e));
+        timeslots = [];
+        renderTimeslots();
+        hideScheduleForm();
+    } else {
+        // 기존 일반 일정 및 변동 일정 로직 유지
+        if (type === 'variable') {
+            const variableProperties = initFixAndVariableProperties(startDate);
+            const { startDateTime, endDateTime } = variableProperties;
 
+            event.start = startDateTime;
+            event.end = endDateTime;
 
-    //일반 일정
-    else if (type === 'normal') {
-        const normalProperties = initFixAndVariableProperties(startDate, endDate);
-        //TODO initNormalProperties로 변경
-        const { startDateTime, endDateTime } = normalProperties;
+            calendar.addEvent(event);
+            addToDoItem(event);
+        } else if (type === 'normal') {
+            const normalProperties = initFixAndVariableProperties(startDate);
+            const { startDateTime, endDateTime } = normalProperties;
 
-        const frequency = document.getElementById('schedule-frequency').value;
-        const bufferTime = document.getElementById('schedule-buffer-time').value;
-        const weekdays = Array.from(document.querySelectorAll('input[name="weekdays"]:checked')).map(checkbox => checkbox.value);
+            const frequency = document.getElementById('schedule-frequency').value;
+            const bufferTime = document.getElementById('schedule-buffer-time').value;
+            const weekdays = Array.from(document.querySelectorAll('input[name="weekdays"]:checked')).map(checkbox => checkbox.value);
 
-        // TODO: 서버로 요청 전송, 서버에서 생성한 랜덤시간을 받아서 랜덤시간 주입해야 함
+            event.start = startDateTime;
+            event.end = endDateTime;
+            event.rrule = {
+                freq: frequency.toUpperCase(),
+                interval: 1,
+                byweekday: weekdays,
+                dtstart: startDateTime,
+                until: endDateTime
+            };
+            event.duration = '01:00';
+            event.bufferTime = bufferTime;
 
-        event.start = startDateTime; // + 랜덤 시간
-        event.end = endDateTime; // + 랜덤 시간
-        event.rrule = {
-            freq: frequency.toUpperCase(),
-            interval: 1,
-            byweekday: weekdays,
-            dtstart: startDateTime,
-            until: endDateTime
-        };
-        event.duration = '01:00';
-        event.bufferTime = bufferTime;
-        event.type = type;
-
-        calendar.addEvent(event);
-        addToDoItem(event); // 일정 추가 시 To-Do 리스트에 반영
+            calendar.addEvent(event);
+            addToDoItem(event);
+        }
     }
     hideScheduleForm();
 }
+
 
 function hideScheduleForm() {
     document.getElementById('schedule-form-container').style.display = 'none';
@@ -108,14 +102,14 @@ function initCommonProperties() {
     const title = document.getElementById('schedule-title').value;
     const startDate = document.getElementById('schedule-start-date').value;
     const endDate = document.getElementById('schedule-end-date').value;
-    const commonDescription = document.getElementById('schedule-description').value;
+    const description = document.getElementById('schedule-description').value;
 
     return {
         type,
         title,
         startDate,
         endDate,
-        commonDescription
+        description
     };
 }
 
@@ -133,3 +127,24 @@ function initFixAndVariableProperties(startDate, endDate) {
         endDateTime
     };
 }
+
+
+// //TODO 구현해야함
+// function initNormalProperties(){
+//     // const startHour = document.getElementById('schedule-start-hour').value;
+//     // const startMinute = document.getElementById('schedule-start-minute').value;
+//     // const endHour = document.getElementById('schedule-end-hour').value;
+//     // const endMinute = document.getElementById('schedule-end-minute').value;
+//
+//     const startDateTime = convertTo24HourFormat(startDate, startHour, startMinute);
+//     const endDateTime = convertTo24HourFormat(endDate, endHour, endMinute);
+//
+//     return {
+//         startDateTime,
+//         endDateTime
+//     };
+// }
+//
+//
+//
+
