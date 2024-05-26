ee9bb77ee63ef7d2ec146fa6afb24ac7af062546
Woosang An <dntkd857@soongsil.ac.kr>
2024-05-26 22:21:48 +0900

    [Feat] 고정일정 - 종료시간 계산
    종료시간 계산함수 추가

--- src/main/resources/static/js/home/utils/time-option.js
+++ src/main/resources/static/js/home/utils/time-option.js
@@ -9,6 +9,13 @@ function initializeBufferTimeOptions() {
     fillBufferTimeSelect('schedule-buffer-hour', 'schedule-buffer-minute'); // 추가된 희망 여유 시간 필드 초기화
 }
 
+// 고정 일정은 종료시간이 아닌 duration을 받기 때문에 따로 처리 해주는 함수
+// 시간, 분 단위로 초기화
+function initializeFixedTimeOptions() {
+    fillTimeSelect('schedule-start-hour', 'schedule-start-minute');
+    fillBufferTimeSelect('schedule-duration-hour', 'schedule-duration-minute');
+}
+
 function fillTimeSelect(hourId, minuteId) {
     const hourSelect = document.getElementById(hourId);
     const minuteSelect = document.getElementById(minuteId);
@@ -42,4 +49,25 @@ function fillBufferTimeSelect(hourId, minuteId) {
 function convertTo24HourFormat(date, hour, minute) {
     hour = parseInt(hour) % 12 + (hour >= 12 ? 12 : 0); // Convert hour to 24-hour format
     return `${date}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:00`;
+}
+
+function calculateEndTime(startPeriod, startHour, startMinute, durationHour, durationMinute) {
+    startHour = parseInt(startHour);
+    startMinute = parseInt(startMinute);
+    durationHour = parseInt(durationHour);
+    durationMinute = parseInt(durationMinute);
+    if (startPeriod === 'PM' && startHour !== 12) {
+        startHour += 12;
+    } else if (startPeriod === 'AM' && startHour === 12) {
+        startHour = 0;
+    }
+    let startTimeInMinutes = (startHour * 60) + startMinute;
+    const durationInMinutes = (durationHour * 60) + durationMinute;
+    let endTimeInMinutes = startTimeInMinutes + durationInMinutes;
+    let endHour = Math.floor(endTimeInMinutes / 60) % 24; // 24시간을 넘어갈 경우를 대비하여 % 24 사용
+    let endMinute = endTimeInMinutes % 60;
+
+    const formattedEndHour = endHour < 10 ? `0${endHour}` : endHour;
+    const formattedEndMinute = endMinute < 10 ? `0${endMinute}` : endMinute;
+    return `${formattedEndHour}:${formattedEndMinute}:00`;
 }
