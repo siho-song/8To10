bb93678dd95644a81668c10678a8ec8c5cea163d
siho-song <thdtlgh234@naver.com>
2024-05-28 20:57:18 +0900

    [Feat] schedule 커스텀 리포지토리 개발

---
+++ src/main/java/show/schedulemanagement/repository/schedule/ScheduleRepositoryCustom.java
@@ -0,0 +1,13 @@
+package show.schedulemanagement.repository.schedule;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.domain.schedule.Schedule;
+
+public interface ScheduleRepositoryCustom {
+
+    List<Schedule> findAll();
+    List<Schedule> findAllBetweenStartAndEnd(Member member, LocalDateTime start, LocalDateTime end);
+}
