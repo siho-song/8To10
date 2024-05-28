18b010a3d141c368427fe3a27ae14668e5926a11
siho-song <thdtlgh234@naver.com>
2024-05-29 05:13:42 +0900

    [Refactor] 접근 제어 지시자 변경

--- src/main/java/show/schedulemanagement/dto/schedule/ScheduleColor.java
+++ src/main/java/show/schedulemanagement/dto/schedule/ScheduleColor.java
@@ -3,7 +3,7 @@ package show.schedulemanagement.dto.schedule;
 public enum ScheduleColor {
     VARIABLE("#f4511e"), NORMAL("#4CAF50"), FIXED("#3788d8");
 
-    final String hexCode;
+    public final String hexCode;
 
     ScheduleColor(String hexCode) {
         this.hexCode = hexCode;
