427453b14ccb99bd353d724309713ca224b8fc89
siho-song <thdtlgh234@naver.com>
2024-05-28 20:55:01 +0900

    [Refactor] 코드 리팩토링

--- src/main/java/show/schedulemanagement/controller/HomeController.java
+++ src/main/java/show/schedulemanagement/controller/HomeController.java
@@ -1,20 +1,23 @@
 package show.schedulemanagement.controller;
 
-import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import show.schedulemanagement.dto.member.MemberDto;
+import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.service.MemberService;
 
 @Controller
 @RequestMapping("/home")
+@RequiredArgsConstructor
 @Slf4j
 public class HomeController {
+    private final MemberService memberService;
     @GetMapping
-    public String home(HttpSession httpSession , Model model){
-        MemberDto member = (MemberDto) httpSession.getAttribute("member");
+    public String home(Model model){
+        Member member = memberService.getAuthenticatedMember();
         log.debug("HomeController home() : {}} ", member);
         model.addAttribute("member", member);
         return "home/fullcalendar";
--- src/main/java/show/schedulemanagement/controller/schedule/VScheduleController.java
+++ src/main/java/show/schedulemanagement/controller/schedule/VScheduleController.java
@@ -1,6 +1,6 @@
-package show.schedulemanagement.controller;
+package show.schedulemanagement.controller.schedule;
 
-import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.domain.schedule.vSchedule.VSchedule;
-import show.schedulemanagement.dto.member.MemberDto;
-import show.schedulemanagement.dto.schedule.request.VariableRequestDto;
+import show.schedulemanagement.dto.schedule.request.VariableAddDto;
 import show.schedulemanagement.dto.schedule.response.ScheduleResponseDto;
 import show.schedulemanagement.dto.schedule.response.VariableResponseDto;
 import show.schedulemanagement.service.MemberService;
@@ -21,26 +21,28 @@ import show.schedulemanagement.service.schedule.ScheduleService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/schedule/variable")
 @Slf4j
-public class ScheduleController {
+public class VScheduleController {
 
     private final ScheduleService scheduleService;
     private final MemberService memberService;
 
-    @PostMapping("/schedule/variable/add")
-    public ResponseEntity<ScheduleResponseDto> addSchedule(HttpSession session, @RequestBody VariableRequestDto dto){
 
-        log.debug("ScheduleController addSchedule call VariableRequestDto : {}", dto);
-        MemberDto memberDto = (MemberDto) session.getAttribute("member");
-        Member member = memberService.findByEmail(memberDto.getEmail());
+    @PostMapping("/add")
+    public ResponseEntity<ScheduleResponseDto> addSchedule(@RequestBody @Valid VariableAddDto dto){
+
+        //TODO 일정이 있으면 해당일정을 삭제하고 덮는다 .
+        log.debug("VScheduleController addSchedule call VariableAddDto : {}", dto);
+        Member member = memberService.getAuthenticatedMember();
         VSchedule schedule = VSchedule.createVSchedule(member, dto);
         scheduleService.save(schedule);
 
         return new ResponseEntity<>(new VariableResponseDto(schedule), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/schedule/variable/{id}")
-    public ResponseEntity<Long> deleteSchedule(@PathVariable("id") Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> deleteSchedule(@PathVariable("id") Long id) throws Exception {
         scheduleService.deleteById(id);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
