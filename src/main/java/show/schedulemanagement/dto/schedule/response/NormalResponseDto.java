45ae28b201aa9cdcd935a6e81d7d4ee3b740b93a
siho-song <thdtlgh234@naver.com>
2024-05-29 17:01:42 +0900

    [Feat] dto 생성자 구현

--- src/main/java/show/schedulemanagement/dto/schedule/response/NormalResponseDto.java
+++ src/main/java/show/schedulemanagement/dto/schedule/response/NormalResponseDto.java
@@ -1,13 +1,34 @@
 package show.schedulemanagement.dto.schedule.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import show.schedulemanagement.domain.schedule.fSchedule.FSchedule;
+import show.schedulemanagement.domain.schedule.fSchedule.FScheduleDetail;
+import show.schedulemanagement.domain.schedule.nSchedule.NSchedule;
+import show.schedulemanagement.domain.schedule.nSchedule.NScheduleDetail;
+import show.schedulemanagement.dto.schedule.ScheduleColor;
 
 @SuperBuilder
 @Getter
 @ToString(callSuper = true)
+@NoArgsConstructor
 public class NormalResponseDto extends ScheduleResponseDto {
     private Long parentId;
     private Integer dailyAmount;
+
+    public NormalResponseDto(NSchedule nSchedule, NScheduleDetail nScheduleDetail) {
+        super(nScheduleDetail.getId(),
+                nSchedule.getTitle(),
+                nSchedule.getCommonDescription(),
+                nScheduleDetail.getStartDate(),
+                nScheduleDetail.getEndDate(),
+                "normal",
+                ScheduleColor.NORMAL.hexCode,
+                nScheduleDetail.isCompleteStatus());
+
+        this.parentId = nSchedule.getId();
+        this.dailyAmount = nScheduleDetail.getDailyAmount();
+    }
 }
