7f458b33ab1b2c6d66f6d55fa5881633b220e4d3
Woosang An <dntkd857@soongsil.ac.kr>
2024-07-09 17:23:53 +0900

    [Feat] 일정 제출 폼 로직 수정
    - 서버 통신 코드 수정
    - 폼에서 넘어가는 값이 잘못되어 수정(요일 MO -> mo 등)

--- src/main/resources/static/js/home/form/scheduleCategoryForm.js
+++ src/main/resources/static/js/home/form/scheduleCategoryForm.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function createScheduleForm(type) {
+    document.getElementById('schedule-form-header').textContent = "";
+
     document.getElementById('schedule-type-popup').style.display = 'none';
     document.getElementById('schedule-form-container').style.display = 'block';
 
@@ -16,6 +18,7 @@ function createScheduleForm(type) {
     initializeInputField();
 
     if (type === 'fixed') {  <!-- 고정 일정-->
+        document.getElementById('schedule-form-header').textContent = "고정 일정";
         document.getElementById('add-timeslot-btn').style.display = 'block';
 
         additionalFields.innerHTML = `
@@ -56,13 +59,13 @@ function createScheduleForm(type) {
             <div class="form-group">
                 <label>수행 요일</label>
                 <div class="weekday-checkbox-group">
-                    <label for="weekday-mo"><input type="checkbox" id="weekday-mo" name="days" value="MO">월</label>
-                    <label for="weekday-tu"><input type="checkbox" id="weekday-tu" name="days" value="TU">화</label>
-                    <label for="weekday-we"><input type="checkbox" id="weekday-we" name="days" value="WE">수</label>
-                    <label for="weekday-th"><input type="checkbox" id="weekday-th" name="days" value="TH">목</label>
-                    <label for="weekday-fr"><input type="checkbox" id="weekday-fr" name="days" value="FR">금</label>
-                    <label for="weekday-sa"><input type="checkbox" id="weekday-sa" name="days" value="SA">토</label>
-                    <label for="weekday-su"><input type="checkbox" id="weekday-su" name="days" value="SU">일</label>
+                    <label for="weekday-mo"><input type="checkbox" id="weekday-mo" name="days" value="mo">월</label>
+                    <label for="weekday-tu"><input type="checkbox" id="weekday-tu" name="days" value="tu">화</label>
+                    <label for="weekday-we"><input type="checkbox" id="weekday-we" name="days" value="we">수</label>
+                    <label for="weekday-th"><input type="checkbox" id="weekday-th" name="days" value="th">목</label>
+                    <label for="weekday-fr"><input type="checkbox" id="weekday-fr" name="days" value="fr">금</label>
+                    <label for="weekday-sa"><input type="checkbox" id="weekday-sa" name="days" value="sa">토</label>
+                    <label for="weekday-su"><input type="checkbox" id="weekday-su" name="days" value="su">일</label>
                 </div>
             </div>
             <div class="form-group">
@@ -75,33 +78,52 @@ function createScheduleForm(type) {
         handleFrequencyChange(); // 초기 빈도 값 설정
     }
     else if (type === 'normal') {   <!-- 일반 일정-->
+        document.getElementById('schedule-form-header').textContent = "일반 일정";
         document.getElementById('add-timeslot-btn').style.display = 'none';
         additionalFields.innerHTML = `
             <div class="form-group">
-                    <label for="schedule-buffer-time">희망 여유 시간</label>
-                    <select id="schedule-buffer-hour" name="bufferHour">
-                        <!-- 시간 옵션 -->
-                    </select>
-                    <select id="schedule-buffer-minute" name="bufferMinute">
-                        <!-- 분 옵션 -->
-                    </select>
-            </div>
-            <div class="form-group">
-                <label for="schedule-days">수행 요일</label>
-                <div class="weekday-checkbox-group">
-                        <label for="weekday-mo"><input type="checkbox" id="weekday-mo" name="days" value="mo" checked> 월</label>
-                        <label for="weekday-tu"><input type="checkbox" id="weekday-tu" name="days" value="tu" checked> 화</label>
-                        <label for="weekday-we"><input type="checkbox" id="weekday-we" name="days" value="we" checked> 수</label>
-                        <label for="weekday-th"><input type="checkbox" id="weekday-th" name="days" value="th" checked> 목</label>
-                        <label for="weekday-fr"><input type="checkbox" id="weekday-fr" name="days" value="fr" checked> 금</label>
-                        <label for="weekday-sa"><input type="checkbox" id="weekday-sa" name="days" value="sa" checked> 토</label>
-                        <label for="weekday-su"><input type="checkbox" id="weekday-su" name="days" value="su" checked> 일</label>
+                <label for="schedule-buffer-time">희망 여유 시간</label>
+                <div class="time-input-group" id="buffer-time-input-group">
+                    <select id="schedule-buffer-hour" name="bufferHour"></select>
+                    <select id="schedule-buffer-minute" name="bufferMinute"></select>
                 </div>
             </div>
+            <div class="form-group">
+                <label for="schedule-total-amount">목표 달성 총량</label>
+                <input type="text" id="schedule-total-amount" name="totalAmount" placeholder="달성하고 싶은 양">
+            </div>
+            <div class="form-group">
+                <label for="schedule-perform-in-day">희망 하루 수행시간</label>
+                <div class="time-input-group" id="perform-in-day-input-group">
+                    <select id="schedule-perform-hour" name="performHour"></select>
+                    <select id="schedule-perform-minute" name="performMinute"></select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="schedule-perform-in-week">희망 주간 수행빈도</label>
+                <select id="schedule-perform-in-week" name="performInWeek">
+                    <option value="1">1회</option>
+                    <option value="2">2회</option>
+                    <option value="3">3회</option>
+                    <option value="4">4회</option>
+                    <option value="5">5회</option>
+                    <option value="6">6회</option>
+                    <option value="7">7회</option>
+                </select>
+            </div>
+            <div class="form-group">
+            <label for="include-weekend">주말 포함 여부</label>
+            <div>
+                <label><input type="checkbox" id="include-saturday" name="includeSaturday"> 토요일 포함</label>
+                <label><input type="checkbox" id="include-sunday" name="includeSunday"> 일요일 포함</label>
+            </div>
+        </div>
         `;
-        initializeBufferTimeOptions();
+        initializeNormalTimeOptions();
     }
+
     else if (type === 'variable') {   <!-- 변동 일정-->
+        document.getElementById('schedule-form-header').textContent = "변동 일정";
         document.getElementById('add-timeslot-btn').style.display = 'none';
         additionalFields.innerHTML = `
             <div class="form-group">
