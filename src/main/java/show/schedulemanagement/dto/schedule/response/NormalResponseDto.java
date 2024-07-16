922ce845d06ffcebba21de76580553270fd2ee00
siho-song <thdtlgh234@naver.com>
2024-07-17 00:43:13 +0900

    [Feat] 생성자 수정
    생성된 일정의 여유시간을 제외한 시간을 반환하게 변경

--- src/main/java/show/schedulemanagement/dto/schedule/response/NormalResponseDto.java
+++ src/main/java/show/schedulemanagement/dto/schedule/response/NormalResponseDto.java
@@ -1,5 +1,7 @@
 package show.schedulemanagement.dto.schedule.response;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -18,12 +20,14 @@ public class NormalResponseDto extends ScheduleResponseDto {
     private Long parentId;
     private String detailDescription;
     private Double dailyAmount;
+    private LocalTime bufferTime;
 
     public NormalResponseDto(NSchedule nSchedule, NScheduleDetail nScheduleDetail) {
         super(nScheduleDetail.getId(),
                 nSchedule.getTitle(),
                 nSchedule.getCommonDescription(),
-                nScheduleDetail.getStartDate(),
+                nScheduleDetail.getStartDate().plusHours(nSchedule.getBufferTime().getHour())
+                        .plusMinutes(nSchedule.getBufferTime().getMinute()),
                 nScheduleDetail.getEndDate(),
                 "normal",
                 ScheduleColor.NORMAL.hexCode,
@@ -32,5 +36,6 @@ public class NormalResponseDto extends ScheduleResponseDto {
         this.parentId = nSchedule.getId();
         this.dailyAmount = nScheduleDetail.getDailyAmount();
         this.detailDescription = nSchedule.getCommonDescription();
+        this.bufferTime = nSchedule.getBufferTime();
     }
 }
