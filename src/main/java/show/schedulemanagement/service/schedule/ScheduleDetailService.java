46caa0cd9ed4c432e47964f2555b7ce8bbaeb5f6
siho-song <thdtlgh234@naver.com>
2024-07-27 21:58:27 +0900

    [Feat] 일반일정 Detail 조회 함수 선언

--- src/main/java/show/schedulemanagement/service/schedule/ScheduleDetailService.java
+++ src/main/java/show/schedulemanagement/service/schedule/ScheduleDetailService.java
@@ -1,8 +1,10 @@
 package show.schedulemanagement.service.schedule;
 
 import show.schedulemanagement.domain.schedule.fSchedule.FScheduleDetail;
+import show.schedulemanagement.domain.schedule.nSchedule.NScheduleDetail;
 
 public interface ScheduleDetailService {
     void deleteFdById(Long id);
     FScheduleDetail findFdById(Long id);
+    NScheduleDetail findNdById(Long id);
 }
