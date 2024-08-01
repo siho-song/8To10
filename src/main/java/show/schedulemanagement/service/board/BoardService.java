1842c92eea3141a2ed9f32ac8b5c0218140e3204
siho-song <thdtlgh234@naver.com>
2024-08-01 18:49:13 +0900

    [Feat] 게시판 Service Impl

---
+++ src/main/java/show/schedulemanagement/service/board/BoardService.java
@@ -0,0 +1,8 @@
+package show.schedulemanagement.service.board;
+
+import show.schedulemanagement.domain.board.Board;
+import show.schedulemanagement.dto.board.BoardRequestDto;
+
+public interface BoardService {
+    void save(Board board);
+}
