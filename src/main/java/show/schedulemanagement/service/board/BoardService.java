3f9710e4812596c13f5de88fd3dde316bee17c0a
siho-song <thdtlgh234@naver.com>
2024-08-03 23:38:36 +0900

    [Style] 미사용 import문 삭제

--- src/main/java/show/schedulemanagement/service/board/BoardService.java
+++ src/main/java/show/schedulemanagement/service/board/BoardService.java
@@ -1,7 +1,6 @@
 package show.schedulemanagement.service.board;
 
 import show.schedulemanagement.domain.board.Board;
-import show.schedulemanagement.dto.board.BoardRequestDto;
 
 public interface BoardService {
     void save(Board board);
