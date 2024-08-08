dbdfd0fbe154678551d3d3f8e4860d5a7fc6d38a
siho-song <thdtlgh234@naver.com>
2024-08-09 01:25:08 +0900

    [Feat] 게시글 삭제 엔드포인트 구현

--- src/main/java/show/schedulemanagement/controller/board/BoardController.java
+++ src/main/java/show/schedulemanagement/controller/board/BoardController.java
@@ -1,5 +1,6 @@
 package show.schedulemanagement.controller.board;
 
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.*;
 
 import lombok.RequiredArgsConstructor;
@@ -7,9 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,19 +41,26 @@ public class BoardController {
         boardService.save(board);
         BoardResponseDto responseDto = BoardResponseDto.from(board);
 
-        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+        return new ResponseEntity<>(responseDto, CREATED);
     }
 
     @GetMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Page<BoardResponseDto>> getBoardPage(@RequestBody BoardSearchCond searchCond) {
         Pageable pageable = PageRequest.of(searchCond.getPageNum(), searchCond.getPageSize());
         Page<BoardResponseDto> result = boardService.searchBoardPage(searchCond, pageable);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        return new ResponseEntity<>(result, OK);
     }
 
     @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<BoardResponseDto> getBoard(@PathVariable(name = "id") Long id){
-        Board board = boardService.findById(id);
-        return new ResponseEntity<>(BoardResponseDto.from(board), HttpStatus.OK);
+        Board board = boardService.findByIdWithReplies(id);
+        return new ResponseEntity<>(BoardResponseDto.from(board), OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteBoard(@PathVariable(name = "id") Long id){
+        Member member = memberService.getAuthenticatedMember();
+        boardService.deleteById(member, id);
+        return new ResponseEntity<>(id, OK);
     }
 }
