be6ddf3c86319334aab7a867b55dd9c894446496
siho-song <thdtlgh234@naver.com>
2024-08-08 20:27:25 +0900

    [Feat] 게시글 페이지 조회 서비스 구현

--- src/main/java/show/schedulemanagement/service/board/BoardService.java
+++ src/main/java/show/schedulemanagement/service/board/BoardService.java
@@ -1,7 +1,12 @@
 package show.schedulemanagement.service.board;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import show.schedulemanagement.domain.board.Board;
+import show.schedulemanagement.dto.board.BoardResponseDto;
+import show.schedulemanagement.dto.board.BoardSearchCond;
 
 public interface BoardService {
     void save(Board board);
+    Page<BoardResponseDto> searchBoardPage(BoardSearchCond searchCond, Pageable pageable);
 }
--- src/main/java/show/schedulemanagement/service/board/BoardServiceImpl.java
+++ src/main/java/show/schedulemanagement/service/board/BoardServiceImpl.java
@@ -2,9 +2,13 @@ package show.schedulemanagement.service.board;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import show.schedulemanagement.domain.board.Board;
+import show.schedulemanagement.dto.board.BoardResponseDto;
+import show.schedulemanagement.dto.board.BoardSearchCond;
 import show.schedulemanagement.repository.board.BoardRepository;
 
 @Service
@@ -19,5 +23,10 @@ public class BoardServiceImpl implements BoardService {
     @Transactional
     public void save(Board board) {
         boardRepository.save(board);
+    }
+
+    @Override
+    public Page<BoardResponseDto> searchBoardPage(BoardSearchCond searchCond, Pageable pageable) {
+        return boardRepository.searchPage(searchCond, pageable);
     }
 }
