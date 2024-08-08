bd1dca355a996d9f3e4aec95aa852e1e1074c3be
siho-song <thdtlgh234@naver.com>
2024-08-09 01:15:03 +0900

    [Feat] 게시글 삭제 서비스 구현
    +findById-> findByIdWithReplies 메서드명 변경

--- src/main/java/show/schedulemanagement/service/board/BoardServiceImpl.java
+++ src/main/java/show/schedulemanagement/service/board/BoardServiceImpl.java
@@ -1,5 +1,6 @@
 package show.schedulemanagement.service.board;
 
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +10,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import show.schedulemanagement.domain.board.Board;
+import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.board.BoardResponseDto;
 import show.schedulemanagement.dto.board.BoardSearchCond;
+import show.schedulemanagement.repository.board.BoardHeartsRepository;
 import show.schedulemanagement.repository.board.BoardRepository;
+import show.schedulemanagement.repository.board.BoardScrapRepository;
+import show.schedulemanagement.repository.board.reply.ReplyHeartsRepository;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,6 +25,9 @@ import show.schedulemanagement.repository.board.BoardRepository;
 public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
+    private final BoardHeartsRepository boardHeartsRepository;
+    private final BoardScrapRepository boardScrapRepository;
+    private final ReplyHeartsRepository replyHeartsRepository;
 
     @Override
     @Transactional
@@ -28,7 +36,7 @@ public class BoardServiceImpl implements BoardService {
     }
 
     @Override
-    public Board findById(Long id) {
+    public Board findByIdWithReplies(Long id) {
         Optional<Board> board = boardRepository.findByIdWithReplies(id);
         return board.orElseThrow(() -> new EntityNotFoundException("해당 게시글을 찾을 수 없습니다."));
     }
@@ -36,5 +44,18 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public Page<BoardResponseDto> searchBoardPage(BoardSearchCond searchCond, Pageable pageable) {
         return boardRepository.searchPage(searchCond, pageable);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(Member member, Long id) {
+        Board board = findByIdWithReplies(id);
+        String createdBy = board.getMember().getEmail();
+        if(member.getEmail().equals(createdBy)){
+            boardHeartsRepository.deleteHeartsByBoardId(board);
+            boardScrapRepository.deleteScrapByBoard(board);
+            replyHeartsRepository.deleteByReplies(board.getReplies());
+            boardRepository.delete(board);
+        }
     }
 }
