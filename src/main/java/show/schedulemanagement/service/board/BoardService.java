600b33a7cc415547d991db79d66d990b08ed031d
siho-song <thdtlgh234@naver.com>
2024-08-11 17:42:22 +0900

    [Feat] 멤버+게시글조회 서비스 구현
    1. findByIdWithReplies -> findByIdWithRepliesAndMember 로 메서드명 변경

--- src/main/java/show/schedulemanagement/service/board/BoardService.java
+++ src/main/java/show/schedulemanagement/service/board/BoardService.java
@@ -6,11 +6,13 @@ import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.board.BoardPageResponse;
 import show.schedulemanagement.dto.board.BoardPageRequest;
+import show.schedulemanagement.dto.board.BoardUpdateRequest;
 
 public interface BoardService {
     void save(Board board);
     Board findById(Long id);
-    Board findByIdWithReplies(Long id);
+    Board findByIdWithMember(Long id);
+    Board findByIdWithRepliesAndMember(Long id);
     Page<BoardPageResponse> searchBoardPage(BoardPageRequest searchCond, Pageable pageable);
     void deleteById(Member member, Long id);
 }
--- src/main/java/show/schedulemanagement/service/board/BoardServiceImpl.java
+++ src/main/java/show/schedulemanagement/service/board/BoardServiceImpl.java
@@ -12,6 +12,7 @@ import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.board.BoardPageResponse;
 import show.schedulemanagement.dto.board.BoardPageRequest;
+import show.schedulemanagement.dto.board.BoardUpdateRequest;
 import show.schedulemanagement.repository.board.BoardHeartsRepository;
 import show.schedulemanagement.repository.board.BoardRepository;
 import show.schedulemanagement.repository.board.BoardScrapRepository;
@@ -40,8 +41,13 @@ public class BoardServiceImpl implements BoardService {
     }
 
     @Override
-    public Board findByIdWithReplies(Long id) {
-        Optional<Board> board = boardRepository.findByIdWithReplies(id);
+    public Board findByIdWithMember(Long id) {
+        return boardRepository.findByIdWithMember(id).orElseThrow(()->new EntityNotFoundException("해당 게시글을 찾을 수 없습니다."));
+    }
+
+    @Override
+    public Board findByIdWithRepliesAndMember(Long id) {
+        Optional<Board> board = boardRepository.findByIdWithRepliesAndMember(id);
         return board.orElseThrow(() -> new EntityNotFoundException("해당 게시글을 찾을 수 없습니다."));
     }
 
@@ -53,7 +59,7 @@ public class BoardServiceImpl implements BoardService {
     @Override
     @Transactional
     public void deleteById(Member member, Long id) {
-        Board board = findByIdWithReplies(id);
+        Board board = findByIdWithRepliesAndMember(id);
         String createdBy = board.getMember().getEmail();
         if(member.getEmail().equals(createdBy)){
             boardHeartsRepository.deleteHeartsByBoard(board);
