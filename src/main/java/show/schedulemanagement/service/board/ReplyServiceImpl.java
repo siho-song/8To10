d85ae6e23cf893aa81f92ab506e0fe17926552ce
siho-song <thdtlgh234@naver.com>
2024-08-12 19:11:14 +0900

    [Feat] 댓글,대댓글 등록 서비스 구현

--- src/main/java/show/schedulemanagement/service/board/ReplyServiceImpl.java
+++ src/main/java/show/schedulemanagement/service/board/ReplyServiceImpl.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.board.reply.Reply;
+import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.dto.board.reply.ReplySaveRequest;
 import show.schedulemanagement.repository.board.reply.ReplyRepository;
 
 @Service
@@ -15,6 +18,7 @@ import show.schedulemanagement.repository.board.reply.ReplyRepository;
 public class ReplyServiceImpl implements ReplyService{
 
     private final ReplyRepository replyRepository;
+    private final BoardService boardService;
 
     @Override
     public Reply findById(Long id) {
@@ -24,5 +28,38 @@ public class ReplyServiceImpl implements ReplyService{
     @Override
     public Reply findByIdWithParent(Long id) {
         return replyRepository.findByIdWithParent(id).orElseThrow(() -> new EntityNotFoundException("해당 댓글을 찾을 수 없습니다."));
+    }
+
+    @Override
+    @Transactional
+    public Reply save(ReplySaveRequest request, Member member) {
+        Board board = boardService.findById(request.getBoardId());
+        Long parentId = request.getParentId();
+
+        if(parentId != null){
+            Reply parent = findByIdWithParent(parentId);
+            if(checkEqualBoard(parent.getBoard(), board) && checkLevel(parent)){
+                Reply child = Reply.from(parent, request.getContents(), member, board);
+                replyRepository.save(child);
+                return child;
+            }
+        }
+        Reply reply = Reply.from(null, request.getContents(), member, board);
+        replyRepository.save(reply);
+        return reply;
+    }
+
+    private boolean checkEqualBoard(Board parentReplyBoard, Board board) {
+        if(parentReplyBoard.getId().equals(board.getId())){
+            return true;
+        }
+        throw new RuntimeException("부모댓글의 게시글과 대댓글의 게시글이 일치하지 않습니다."); //TODO Exception 처리
+    }
+
+    private boolean checkLevel(Reply parent) {
+        if (parent.getParent() == null){
+            return true;
+        }
+        throw new RuntimeException("대댓글에는 댓글을 달 수 없습니다."); //TODO Exception 처리
     }
 }
