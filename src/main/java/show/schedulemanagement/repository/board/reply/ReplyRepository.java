11561a1f9095e74813581f50987e6b51edf6f1a2
siho-song <thdtlgh234@naver.com>
2024-08-13 19:49:29 +0900

    [Feat] 이메일로 댓글 조회 메서드 구현
    게시글과 멤버 함께 조회

--- src/main/java/show/schedulemanagement/repository/board/reply/ReplyRepository.java
+++ src/main/java/show/schedulemanagement/repository/board/reply/ReplyRepository.java
@@ -1,5 +1,6 @@
 package show.schedulemanagement.repository.board.reply;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,8 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @EntityGraph(attributePaths = "parent")
     @Query("select r from Reply r where r.id = :id")
     Optional<Reply> findByIdWithParent(@Param(value = "id") Long id);
+
+    @EntityGraph(attributePaths = {"member", "board"})
+    @Query("select r from Reply  r where r.member.email = :email")
+    List<Reply> findAllWithBoardAndMemberByEmail(@Param(value = "email") String email);
 }
