23e83e0e598222014e5aa5c537b3748b580a9650
siho-song <thdtlgh234@naver.com>
2024-08-18 17:42:55 +0900

    [Feat] 댓글 좋아요 벌크 삭제 서비스 구현

---
+++ src/main/java/show/schedulemanagement/service/board/ReplyHeartsService.java
@@ -0,0 +1,7 @@
+package show.schedulemanagement.service.board;
+
+import show.schedulemanagement.domain.board.reply.Reply;
+
+public interface ReplyHeartsService {
+    void deleteByReply(Reply reply);
+}
