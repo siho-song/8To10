7fc89980a8b7deb286b5462f0d3098d0c0b2046d
siho-song <thdtlgh234@naver.com>
2024-08-18 17:36:07 +0900

    [Feat] 클라이언트의 이메일, 댓글 작성자의 이메일 체크 메서드 구현
    + findAllWithBoardAndMemberByEmail 메서드 위치 변경

--- src/main/java/show/schedulemanagement/service/board/ReplyServiceImpl.java
+++ src/main/java/show/schedulemanagement/service/board/ReplyServiceImpl.java
@@ -10,6 +10,7 @@ import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.board.reply.Reply;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.board.reply.ReplySaveRequest;
+import show.schedulemanagement.repository.board.reply.ReplyHeartsRepository;
 import show.schedulemanagement.repository.board.reply.ReplyRepository;
 
 @Service
@@ -20,6 +21,7 @@ public class ReplyServiceImpl implements ReplyService{
 
     private final ReplyRepository replyRepository;
     private final BoardService boardService;
+    private final ReplyHeartsService replyHeartsService;
 
     @Override
     public Reply findById(Long id) {
@@ -35,6 +37,12 @@ public class ReplyServiceImpl implements ReplyService{
     public Reply findByIdWithMemberAndParent(Long id) {
         return replyRepository.findByIdWithMemberAndParent(id)
                 .orElseThrow(() -> new EntityNotFoundException("해당 댓글을 찾을 수 없습니다."));
+    }
+
+    @Override
+    public List<Reply> findAllWithBoardAndMemberByEmail(Member member) {
+        String email = member.getEmail();
+        return replyRepository.findAllWithBoardAndMemberByEmail(email);
     }
 
     @Override
@@ -56,10 +64,11 @@ public class ReplyServiceImpl implements ReplyService{
         return reply;
     }
 
-    @Override
-    public List<Reply> findAllWithBoardAndMemberByEmail(Member member) {
-        String email = member.getEmail();
-        return replyRepository.findAllWithBoardAndMemberByEmail(email);
+    private boolean equalEmail(Member member, Reply reply) {
+        if(!member.getEmail().equals(reply.getMember().getEmail())){
+            return true;
+        }
+        throw new RuntimeException("댓글 작성자와 클라이언트의 이메일이 일치하지 않습니다.");
     }
 
     private boolean checkEqualBoard(Board parentReplyBoard, Board board) {
