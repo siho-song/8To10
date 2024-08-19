ead8bc3049c8a9a87b8a9a54510a710470a84422
siho-song <thdtlgh234@naver.com>
2024-08-19 10:57:46 +0900

    [Test] 댓글 삭제 서비스 테스트 구현

--- src/test/java/show/schedulemanagement/service/board/ReplyServiceImplTest.java
+++ src/test/java/show/schedulemanagement/service/board/ReplyServiceImplTest.java
@@ -2,6 +2,7 @@ package show.schedulemanagement.service.board;
 
 import static org.assertj.core.api.Assertions.*;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,7 @@ import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.service.MemberService;
 
 @SpringBootTest
+@DisplayName("댓글 CRUD")
 class ReplyServiceImplTest {
 
     @Autowired
@@ -26,5 +28,27 @@ class ReplyServiceImplTest {
         Member member = memberService.findByEmail("normal@example.com");
         List<Reply> allByMemberEmail = replyService.findAllWithBoardAndMemberByEmail(member);
         assertThat(allByMemberEmail.size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("댓글 삭제 - 댓글")
+    void deleteReply(){
+        Member member = memberService.findByEmail("normal@example.com");
+        replyService.delete(member,1L);
+        assertThatThrownBy(() -> replyService.findById(1L)).isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("댓글 삭제 - 클라이언트와 작성자 이메일 불일치")
+    void deleteNotEqualEmail(){
+        Member member = memberService.findByEmail("normal@example.com");
+        assertThatThrownBy(() -> replyService.delete(member, 1L)).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("댓글 삭제 - 대댓글")
+    void deleteNestedReply(){
+        Member member = memberService.findByEmail("normal@example.com");
+        replyService.delete(member,14L);
     }
 }
