071f7b779c1c08be797044a1069dfcfc6d8e6b62
siho-song <thdtlgh234@naver.com>
2024-08-21 00:16:46 +0900

    [Feat] 댓글 수정 엔드포인트 구현
    + 메서드 위치 수정

--- src/main/java/show/schedulemanagement/controller/board/reply/ReplyController.java
+++ src/main/java/show/schedulemanagement/controller/board/reply/ReplyController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,8 @@ import show.schedulemanagement.domain.board.reply.Reply;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.board.reply.ReplySaveRequest;
 import show.schedulemanagement.dto.board.reply.ReplySaveResponse;
+import show.schedulemanagement.dto.board.reply.ReplySearchResponse;
+import show.schedulemanagement.dto.board.reply.ReplyUpdateRequest;
 import show.schedulemanagement.service.MemberService;
 import show.schedulemanagement.service.board.ReplyService;
 
@@ -30,12 +33,13 @@ public class ReplyController {
     private final ReplyService replyService;
     private final MemberService memberService;
 
-    @PostMapping(value = "/add", produces = APPLICATION_JSON_VALUE , consumes = APPLICATION_JSON_VALUE)
-    public ResponseEntity<ReplySaveResponse> save(@RequestBody @Valid ReplySaveRequest request){
+    @PatchMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReplySearchResponse> update(@RequestBody @Valid ReplyUpdateRequest request){
         Member member = memberService.getAuthenticatedMember();
-        Reply reply = replyService.save(request, member);
-        ReplySaveResponse response = ReplySaveResponse.from(reply);
-        return new ResponseEntity<>(response, CREATED);
+        replyService.update(member, request);
+        Reply reply = replyService.findByIdWithMemberAndParent(request.getId());
+        ReplySearchResponse responseDto = ReplySearchResponse.from(reply);
+        return new ResponseEntity<>(responseDto, OK);
     }
 
     @DeleteMapping(value = "/{id}")
@@ -43,5 +47,13 @@ public class ReplyController {
         Member member = memberService.getAuthenticatedMember();
         replyService.delete(member,id);
         return new ResponseEntity<>(id, OK);
+    }
+
+    @PostMapping(value = "/add", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReplySaveResponse> save(@RequestBody @Valid ReplySaveRequest request) {
+        Member member = memberService.getAuthenticatedMember();
+        Reply reply = replyService.save(request, member);
+        ReplySaveResponse responseDto = ReplySaveResponse.from(reply);
+        return new ResponseEntity<>(responseDto, CREATED);
     }
 }
