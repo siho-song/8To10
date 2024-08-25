8e01a77adb65c09159ebaa940f919eab92ee85ed
siho-song <thdtlgh234@naver.com>
2024-08-25 18:00:08 +0900

    [Test] 게시글 단건조회 서비스 테스트 구현

--- src/test/java/show/schedulemanagement/service/board/BoardServiceTest.java
+++ src/test/java/show/schedulemanagement/service/board/BoardServiceTest.java
@@ -1,21 +1,26 @@
 package show.schedulemanagement.service.board;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.dto.board.BoardSearchResponse;
 import show.schedulemanagement.security.dto.MemberDetailsDto;
 import show.schedulemanagement.service.MemberService;
 
 @SpringBootTest
+@Transactional
 class BoardServiceTest {
 
     @Autowired
@@ -32,10 +37,23 @@ class BoardServiceTest {
     }
 
     @Test
-    @Transactional
+    @DisplayName("댓글 등록자와 삭제요청한 클라이언트가 같으면 정상삭제 된다.")
     void deleteById() {
         Member member = memberService.getAuthenticatedMember();
         Long boardId = 1L;
+
         boardService.deleteById(member, boardId);
+
+        assertThatThrownBy(()->boardService.findById(1L)).isInstanceOf(EntityNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("게시글 단건 조회")
+    void searchBoard(){
+        Member member = memberService.getAuthenticatedMember();
+        Long boardId = 1L;
+
+        BoardSearchResponse boardSearchResponse = boardService.searchBoard(1L, member);
+        assertThat(boardSearchResponse).isNotNull();
     }
 }
