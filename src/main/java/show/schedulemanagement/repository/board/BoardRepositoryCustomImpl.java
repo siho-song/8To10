27e28152ed0ae1e311e138a92d0d6161a8129a05
siho-song <thdtlgh234@naver.com>
2024-08-25 17:58:51 +0900

    [Feat] 게시글 단건 조회 메서드 구현

--- src/main/java/show/schedulemanagement/repository/board/BoardRepositoryCustomImpl.java
+++ src/main/java/show/schedulemanagement/repository/board/BoardRepositoryCustomImpl.java
@@ -4,21 +4,33 @@ import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import show.schedulemanagement.domain.board.QBoard;
+import show.schedulemanagement.domain.board.QBoardHeart;
+import show.schedulemanagement.domain.board.QBoardScrap;
+import show.schedulemanagement.domain.board.reply.QReply;
+import show.schedulemanagement.domain.board.reply.QReplyHeart;
+import show.schedulemanagement.domain.board.reply.Reply;
+import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.domain.member.QMember;
-import show.schedulemanagement.dto.board.BoardPageResponse;
 import show.schedulemanagement.dto.board.BoardPageRequest;
+import show.schedulemanagement.dto.board.BoardPageResponse;
+import show.schedulemanagement.dto.board.BoardSearchResponse;
 import show.schedulemanagement.dto.board.SearchCond;
 import show.schedulemanagement.dto.board.SortCondition;
+import show.schedulemanagement.dto.board.reply.ReplySearchResponse;
 
+@Slf4j
 public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
 
     private final EntityManager em;
@@ -26,6 +38,10 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
     private JPAQueryFactory query;
     private QBoard qBoard;
     private QMember qMember;
+    private QReply qReply;
+    private QBoardHeart qBoardHeart;
+    private QBoardScrap qBoardScrap;
+    private QReplyHeart qReplyHeart;
 
     public BoardRepositoryCustomImpl(EntityManager em) {
         this.em = em;
@@ -68,8 +84,66 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
                         containContents(searchCond, keyword),
                         containWriter(searchCond, keyword));
 
-        //정렬  -- paging 쿼리에서 바로 정렬 할 것인가 ? 결과를 받고 따로 app level 에서 정렬 할 것 인가 ?/
         return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
+    }
+
+    @Override
+    public Optional<BoardSearchResponse> searchBoard(Long id, Member member) {
+        List<Reply> boardReplies = query.select(qReply)
+                .from(qReply)
+                .leftJoin(qReply.member)
+                .fetchJoin()
+                .where(qReply.board.id.eq(id))
+                .fetch();
+
+        List<Long> likedReplyIds = query.select(qReplyHeart.reply.id)
+                .from(qReplyHeart)
+                .where(qReplyHeart.reply.in(boardReplies)
+                        .and(qReplyHeart.member.id.eq(member.getId())))
+                .fetch();
+
+        BoardSearchResponse boardSearchResponse = query.select(
+                        Projections.fields(BoardSearchResponse.class,
+                                qBoard.id,
+                                qBoard.title,
+                                qBoard.content,
+                                qMember.nickname,
+                                qMember.email,
+                                qBoard.createdAt,
+                                qBoard.updatedAt,
+                                qBoard.totalLike,
+                                qBoard.totalScrap,
+                                hasLike(id, member).as("hasLike"),
+                                hasScrap(id, member).as("hasScrap")
+                        )
+                ).from(qBoard, qBoard)
+                .leftJoin(qBoard.member, qMember)
+                .on(qBoard.member.id.eq(qMember.id))
+                .where(qBoard.id.eq(id))
+                .fetchOne();
+
+        if (boardSearchResponse != null){
+            boardSearchResponse.setLikedReplyIds(likedReplyIds);
+            boardSearchResponse.setReplies(boardReplies.stream().map(ReplySearchResponse::from).toList());
+        }
+
+        return Optional.ofNullable(boardSearchResponse);
+    }
+
+    private BooleanExpression hasScrap(Long id, Member member) {
+        return JPAExpressions.selectOne()
+                .from(qBoardScrap)
+                .where(qBoardScrap.board.id.eq(id)
+                        .and(qBoardScrap.member.id.eq(member.getId()))
+                ).exists();
+    }
+
+    private BooleanExpression hasLike(Long id, Member member) {
+        return JPAExpressions.selectOne()
+                .from(qBoardHeart)
+                .where(qBoardHeart.board.id.eq(id)
+                        .and(qBoardHeart.member.id.eq(member.getId()))
+                ).exists();
     }
 
     private OrderSpecifier<?>[] sortConds(SortCondition cond){
@@ -109,5 +183,9 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom{
         query = new JPAQueryFactory(em);
         qBoard = QBoard.board;
         qMember = QMember.member;
+        qReply = QReply.reply;
+        qBoardHeart = QBoardHeart.boardHeart;
+        qBoardScrap = QBoardScrap.boardScrap;
+        qReplyHeart = QReplyHeart.replyHeart;
     }
 }
