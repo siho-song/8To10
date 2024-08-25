58fe43faf8f47d949739f178455a54c6e4fe3f75
siho-song <thdtlgh234@naver.com>
2024-08-25 19:28:54 +0900

    [Feat] 해당 게시글 멤버의 좋아요 유무 확인

--- src/main/java/show/schedulemanagement/repository/board/BoardHeartRepository.java
+++ src/main/java/show/schedulemanagement/repository/board/BoardHeartRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.board.BoardHeart;
+import show.schedulemanagement.domain.member.Member;
 
 public interface BoardHeartRepository extends JpaRepository<BoardHeart, Long> {
     @Modifying
     @Query("delete from BoardHeart h where h.board = :board")
     void deleteHeartsByBoard(@Param(value = "board") Board board);
+
+    boolean existsBoardHeartByMemberAndBoard(Member member, Board board);
 }
