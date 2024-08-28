8a47cea57b9bad012d01a4c0e8a1258f1175ae98
siho-song <thdtlgh234@naver.com>
2024-08-28 17:20:43 +0900

    [Feat] 게시글 좋아요 제거 서비스 구현

--- src/main/java/show/schedulemanagement/service/board/BoardHeartService.java
+++ src/main/java/show/schedulemanagement/service/board/BoardHeartService.java
@@ -4,4 +4,5 @@ import show.schedulemanagement.domain.member.Member;
 
 public interface BoardHeartService {
     void addHeart(Long boardId, Member member);
+    void deleteHeart(Long boardId, Member member);
 }
