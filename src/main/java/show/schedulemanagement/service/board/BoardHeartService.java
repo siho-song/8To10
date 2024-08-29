f556bdeb1ce51ff9dd9c48b53d201f76e75a028a
siho-song <thdtlgh234@naver.com>
2024-08-29 19:22:02 +0900

    [Feat] 게시글 좋아요들 삭제 메서드 구현
    + 메서드명 변경 , 변경된 메서드들 파라미터 변경

--- src/main/java/show/schedulemanagement/service/board/BoardHeartService.java
+++ src/main/java/show/schedulemanagement/service/board/BoardHeartService.java
@@ -1,8 +1,11 @@
 package show.schedulemanagement.service.board;
 
+import show.schedulemanagement.domain.board.Board;
 import show.schedulemanagement.domain.member.Member;
 
 public interface BoardHeartService {
-    void addHeart(Long boardId, Member member);
-    void deleteHeart(Long boardId, Member member);
+    void add(Board board, Member member);
+    void delete(Board board, Member member);
+    void deleteHeartsByBoard(Board board);
+
 }
