9346a6adcf311c744dd999bb4897fbec44d6541d
siho-song <thdtlgh234@naver.com>
2024-08-29 17:38:47 +0900

    [Feat] 댓글 좋아요 추가 메서드 구현

--- src/main/java/show/schedulemanagement/service/board/ReplyHeartService.java
+++ src/main/java/show/schedulemanagement/service/board/ReplyHeartService.java
@@ -2,8 +2,10 @@ package show.schedulemanagement.service.board;
 
 import java.util.List;
 import show.schedulemanagement.domain.board.reply.Reply;
+import show.schedulemanagement.domain.member.Member;
 
 public interface ReplyHeartService {
     void deleteByReply(Reply reply);
     void deleteByReplies(List<Reply> replies);
+    void addHeart(Reply reply, Member member);
 }
