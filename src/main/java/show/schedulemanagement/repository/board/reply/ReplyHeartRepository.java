b98191005af8f4a38549a970510eae8f1a44ba31
siho-song <thdtlgh234@naver.com>
2024-08-29 17:32:27 +0900

    [Feat]  클라이언트의 좋아요 유무 확인 메서드 구현

--- src/main/java/show/schedulemanagement/repository/board/reply/ReplyHeartRepository.java
+++ src/main/java/show/schedulemanagement/repository/board/reply/ReplyHeartRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import show.schedulemanagement.domain.board.reply.Reply;
 import show.schedulemanagement.domain.board.reply.ReplyHeart;
+import show.schedulemanagement.domain.member.Member;
 
 public interface ReplyHeartRepository extends JpaRepository<ReplyHeart, Long> {
 
@@ -17,4 +18,6 @@ public interface ReplyHeartRepository extends JpaRepository<ReplyHeart, Long> {
     @Modifying
     @Query("delete from ReplyHeart rh where rh.reply = :reply")
     void deleteByReply(@Param(value = "reply") Reply reply);
+
+    boolean existsReplyHeartByMemberAndReply(Member member, Reply reply);
 }
