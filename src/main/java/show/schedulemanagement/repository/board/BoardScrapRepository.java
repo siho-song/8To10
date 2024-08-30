104ce75c95eda2384069f59e7938475555bfd39a
siho-song <thdtlgh234@naver.com>
2024-08-30 17:57:23 +0900

    [Feat] 멤버와 게시글로 스크랩 조회 메서드 구현

--- src/main/java/show/schedulemanagement/repository/board/BoardScrapRepository.java
+++ src/main/java/show/schedulemanagement/repository/board/BoardScrapRepository.java
@@ -1,5 +1,6 @@
 package show.schedulemanagement.repository.board;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,9 @@ import show.schedulemanagement.domain.board.BoardScrap;
 import show.schedulemanagement.domain.member.Member;
 
 public interface BoardScrapRepository extends JpaRepository<BoardScrap, Long> {
+
+    Optional<BoardScrap> findByMemberAndBoard(Member member, Board board);
+
     @Modifying
     @Query("delete from BoardScrap s where s.board = :board")
     void deleteScrapByBoard(@Param(value = "board") Board board);
