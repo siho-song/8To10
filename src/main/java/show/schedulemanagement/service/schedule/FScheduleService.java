046362dca670c2733323614d3547dd2dc06c4ea6
siho-song <thdtlgh234@naver.com>
2024-09-02 14:40:06 +0900

    [Feat] 고정일정 생성 서비스 메서드 구현

--- src/main/java/show/schedulemanagement/service/schedule/FScheduleService.java
+++ src/main/java/show/schedulemanagement/service/schedule/FScheduleService.java
@@ -9,17 +9,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import show.schedulemanagement.domain.member.Member;
-import show.schedulemanagement.domain.schedule.Schedule;
-import show.schedulemanagement.domain.schedule.ScheduleAble;
 import show.schedulemanagement.domain.schedule.ScheduleDay;
 import show.schedulemanagement.domain.schedule.fSchedule.FSchedule;
 import show.schedulemanagement.domain.schedule.fSchedule.FScheduleDetail;
-import show.schedulemanagement.domain.schedule.nSchedule.NSchedule;
 import show.schedulemanagement.dto.schedule.request.FixAddDto;
 import show.schedulemanagement.dto.schedule.request.FixDetailAddDto;
-import show.schedulemanagement.dto.schedule.response.FixResponseDto;
-import show.schedulemanagement.dto.Result;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,81 +21,15 @@ import show.schedulemanagement.dto.Result;
 @Slf4j
 public class FScheduleService {
 
-    private final ScheduleService scheduleService;
-
     @Transactional
-    public FSchedule addDetailsToFSchedule(Member member, FixAddDto dto) throws RuntimeException{
-        FSchedule newSchedule = FSchedule.createFSchedule(member, dto);
-        String commonDescription = dto.getCommonDescription();
-        LocalDate startDate = dto.getStartDate();
-        LocalDate endDate = dto.getEndDate();
-        List<FixDetailAddDto> events = dto.getEvents();
-        log.debug("addDetailsToFSchedule dto events : {} ",events);
-
-        addDetailsByFrequency(newSchedule, events, commonDescription, startDate, endDate);
-
-        log.debug("addDetailsToFSchedule after addWeeklyAndDailyDetails newSchedule : {} ",newSchedule);
-        List<ScheduleAble> scheduleAbles = newSchedule.getScheduleAbles();
-        log.debug("addDetailsToFSchedule after newSchedule scheduleAbles : {} ",scheduleAbles);
-
-        //기간내 멤버의 모든 일정 불러오기
-        List<Schedule> allSchedule = scheduleService.findAllWithinDates(member, startDate, endDate);
-        //겹치는 일정이 있는지 검사
-        Schedule conflictSchedule = scheduleService.getConflictSchedule(scheduleAbles,allSchedule);
-
-        log.debug("addDetailsToFSchedule after getConflictSchedule about conflictSchedule: {} ",conflictSchedule);
-
-        //TODO 일정이 겹칠떄
-        if(conflictSchedule != null){
-            if (conflictSchedule instanceof NSchedule) {
-                //나중에 구현
-                //TODO
-                log.debug("conflictSchedule is NSchedule");
-            } else {
-                throw new RuntimeException("겹치는 일정을 조율해야합니다");
-            }
-        }
-
-        return newSchedule;
+    public void addDetailsForEachEvent(FSchedule fSchedule, List<FixDetailAddDto> events) {
+        events.forEach(event -> addDetails(fSchedule, event));
     }
 
-    public Result<FixResponseDto> getResult(FSchedule schedule) {
-        List<FScheduleDetail> fScheduleDetails = schedule.getFScheduleDetails();
-        Result<FixResponseDto> result = new Result<>();
-        List<FixResponseDto> events = result.getEvents();
+    private void addDetails(FSchedule schedule, FixDetailAddDto event) {
 
-        for (FScheduleDetail fScheduleDetail : fScheduleDetails) {
-            events.add(new FixResponseDto(schedule,fScheduleDetail));
-        }
-        log.debug("FScheduleService call getResult : {}",result);
-        return result;
-    }
-
-    private void addDetailsByFrequency(FSchedule newSchedule, List<FixDetailAddDto> events, String commonDescription,
-                                       LocalDate startDate, LocalDate endDate) {
-        log.debug("addDetailsByFrequency : {}" , events);
-        for (FixDetailAddDto event : events) {
-            String frequency = event.getFrequency();
-            List<String> days = event.getDays();
-            //주기 체크
-            if (frequency.equals("weekly") || frequency.equals("daily")) {
-                addDetails(newSchedule, commonDescription, startDate, endDate, event,false);
-            }
-            if (frequency.equals("biweekly")) {
-                addDetails(newSchedule, commonDescription, startDate, endDate, event,true);
-            }
-        }
-    }
-
-    private void addDetails(FSchedule schedule,
-                            String detailDescription,
-                            LocalDate startDate,
-                            LocalDate endDate,
-                            FixDetailAddDto event,
-                            boolean isBiweekly
-    ) {
-
-        LocalDate currentDate = startDate;
+        LocalDate currentDate = schedule.getStartDate().toLocalDate();
+        LocalDate endDate = schedule.getEndDate().toLocalDate();
         List<String> days = event.getDays();
         LocalTime duration = event.getDuration();
         LocalTime startTime = event.getStartTime();
@@ -109,18 +37,14 @@ public class FScheduleService {
         while (!currentDate.isAfter(endDate)) {
             DayOfWeek currentDayOfWeek = currentDate.getDayOfWeek();
             for (String day : days) {
-                DayOfWeek dayOfWeek = ScheduleDay.of(day);
-                if (currentDayOfWeek.equals(dayOfWeek)) {
+                if (currentDayOfWeek.equals(ScheduleDay.of(day))) {
                     LocalDateTime startDateTime = LocalDateTime.of(currentDate, startTime);
                     LocalDateTime endDateTime = startDateTime.plusHours(duration.getHour())
                             .plusMinutes(duration.getMinute());
-                    FScheduleDetail fscheduleDetail = FScheduleDetail.createFscheduleDetail(detailDescription,
-                            startDateTime, endDateTime);
+
+                    FScheduleDetail fscheduleDetail = FScheduleDetail.createFscheduleDetail(startDateTime, endDateTime);
                     fscheduleDetail.setFSchedule(schedule);
                 }
-            }
-            if(isBiweekly && currentDayOfWeek.equals(DayOfWeek.SUNDAY)){
-                currentDate = currentDate.plusDays(7);
             }
             currentDate = currentDate.plusDays(1);
         }
