4d2a9214593343586fd98d606f6460d767aebc76
siho-song <thdtlgh234@naver.com>
2024-09-02 14:41:34 +0900

    [Test] 고정일정 엔드포인트 테스트 구현

--- src/test/java/show/schedulemanagement/controller/schedule/FScheduleControllerTest.java
+++ src/test/java/show/schedulemanagement/controller/schedule/FScheduleControllerTest.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -26,20 +27,23 @@ import show.schedulemanagement.security.utils.TokenUtils;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Slf4j
+@DisplayName("고정일정 엔드포인트")
 class FScheduleControllerTest {
-    Logger logger = LoggerFactory.getLogger(FScheduleControllerTest.class);
 
     @Autowired
     MockMvc mockMvc ;
+
     @Autowired
     ObjectMapper objectMapper;
+
     @Autowired
     TokenUtils tokenUtils;  // TokenUtils 주입받기
 
 
     @Test
-    @DisplayName("올바른 형식의 고정일정 데이터를 삽입하면 테스트가 성공한다.")
-    void addFSchedule() throws Exception {
+    @DisplayName("고정일정 생성 - weekly, biweekly, daily")
+    void add_weekly() throws Exception {
 
         String token = tokenUtils.generateJwtToken(new LoginMemberDto("normal@example.com")); // 토큰 생성
         MockCookie jwtCookie = new MockCookie("jwt", token); // JWT 쿠키 생성
@@ -47,19 +51,21 @@ class FScheduleControllerTest {
         List<FixDetailAddDto> events = new ArrayList<>();
         events.add(getFixDetailAddDto(LocalTime.now(), LocalTime.of(2, 0), "weekly",
                 new ArrayList<>(List.of("mo", "we", "fr"))));
-        events.add(getFixDetailAddDto(LocalTime.now(), LocalTime.of(2, 0), "weekly",
+        events.add(getFixDetailAddDto(LocalTime.now(), LocalTime.of(2, 0), "biweekly",
                 new ArrayList<>(List.of("tu","th"))));
+        events.add(getFixDetailAddDto(LocalTime.now().plusHours(3), LocalTime.of(2, 0), "daily",
+                new ArrayList<>(List.of("mo","tu", "we","th","fr","sa","su"))));
 
         FixAddDto fixAddDto = FixAddDto.builder()
                 .title("테스트 title ")
                 .commonDescription("테스트 commonDescription")
-                .startDate(LocalDate.of(2024, 5, 26))
-                .endDate(LocalDate.of(2024, 6, 7))
+                .startDate(LocalDate.now())
+                .endDate(LocalDate.now().plusMonths(2))
                 .events(events)
                 .build();
 
         String fixAddDtoJson = objectMapper.writeValueAsString(fixAddDto);
-        logger.debug("addFSchedule Test request dto : {} ", fixAddDtoJson);
+        log.debug("addFSchedule Test request dto : {} ", fixAddDtoJson);
 
         //post 요청
         mockMvc.perform(post("/schedule/fixed/add")
