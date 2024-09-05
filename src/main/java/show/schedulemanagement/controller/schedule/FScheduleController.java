1c45655b37bb2ccc342487f6d96d77a73840ea94
siho-song <thdtlgh234@naver.com>
2024-09-06 01:33:41 +0900

    [Feat] 고정일정 자식일정 단건삭제 엔드포인트 구현

--- src/main/java/show/schedulemanagement/controller/schedule/FScheduleController.java
+++ src/main/java/show/schedulemanagement/controller/schedule/FScheduleController.java
@@ -1,11 +1,14 @@
 package show.schedulemanagement.controller.schedule;
 
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.*;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import show.schedulemanagement.dto.schedule.request.FixAddDto;
 import show.schedulemanagement.dto.Result;
 import show.schedulemanagement.dto.schedule.response.ScheduleResponseDto;
 import show.schedulemanagement.service.MemberService;
+import show.schedulemanagement.service.schedule.fSchedule.FScheduleDetailService;
 import show.schedulemanagement.service.schedule.fSchedule.FScheduleService;
 import show.schedulemanagement.service.schedule.ScheduleService;
 
@@ -26,9 +30,10 @@ import show.schedulemanagement.service.schedule.ScheduleService;
 public class FScheduleController {
     private final ScheduleService scheduleService;
     private final FScheduleService fScheduleService;
+    private final FScheduleDetailService fScheduleDetailService;
     private final MemberService memberService;
 
-    @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/add", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Result<ScheduleResponseDto>> add(@RequestBody @Valid FixAddDto dto) {
         Member member = memberService.getAuthenticatedMember();
         FSchedule fSchedule = FSchedule.createFSchedule(member, dto);
@@ -39,6 +44,13 @@ public class FScheduleController {
         Result<ScheduleResponseDto> result = new Result<>();
         scheduleService.setResultFromSchedule(result, fSchedule);
 
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
+        return new ResponseEntity<>(result, CREATED);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> deleteDetail(@PathVariable(value = "id") Long id){
+        Member member = memberService.getAuthenticatedMember();
+        fScheduleDetailService.deleteById(member,id);
+        return new ResponseEntity<>(id, OK);
     }
 }
