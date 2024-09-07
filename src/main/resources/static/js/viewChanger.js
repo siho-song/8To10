9a86d6776a6d8bdf250cc2b0f145033ab5d0d7b0
Woosang An <dntkd857@soongsil.ac.kr>
2024-09-07 20:25:55 +0900

    [Feat] post의 클릭 이벤트에 게시글 상세 뷰로 이동하기 위한 메소드 추가

--- src/main/resources/static/js/viewChanger.js
+++ src/main/resources/static/js/viewChanger.js
@@ -8,6 +8,13 @@ document.addEventListener("click", function (event) {
         event.preventDefault();
         showCreatePostPage();
     }
+
+    // TODO 게시글 상세 뷰 기능이 구현되면 post 프래그먼트의 게시글 id값을 확인하여 게시글 페이지의 세부 사항을 넣어주는 함수를 실행 시켜야 함
+    else if (event.target.id === "post") {
+        event.preventDefault();
+        showPostDetailPage();
+        // TODO 게시글 세부사항을 채워주는 함수 실행
+    }
 });
 
 // TODO 마이페이지, 성취도 페이지가 구현되면 해당 뷰를 담는 container도 비활성화하는 코드 추가 필요
