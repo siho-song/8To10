3f6ca888f02cbf77150672a7cd34c19a9b74f4f5
siho-song <thdtlgh234@naver.com>
2024-09-07 22:50:48 +0900

    [Feat] in절 삭제 메서드 구현

--- src/main/java/show/schedulemanagement/repository/schedule/fSchedule/FScheduleDetailRepository.java
+++ src/main/java/show/schedulemanagement/repository/schedule/fSchedule/FScheduleDetailRepository.java
@@ -3,6 +3,7 @@ package show.schedulemanagement.repository.schedule.fSchedule;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import show.schedulemanagement.domain.schedule.fSchedule.FSchedule;
@@ -13,4 +14,8 @@ public interface FScheduleDetailRepository extends JpaRepository<FScheduleDetail
     List<FScheduleDetail> findByStartDateGEAndEmailAndParent(@Param(value = "start") LocalDateTime start,
                                                              @Param(value = "email") String email,
                                                              @Param(value = "fSchedule") FSchedule fSchedule);
+
+    @Modifying
+    @Query("delete from FScheduleDetail fd where fd in :fScheduleDetails")
+    int deleteByFScheduleDetails(@Param(value = "fScheduleDetails") List<FScheduleDetail> fScheduleDetails);
 }
