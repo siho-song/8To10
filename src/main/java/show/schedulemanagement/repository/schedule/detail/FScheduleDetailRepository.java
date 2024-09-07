6e173b37a6fc1edfd2efb0159afd54d6e37d60fe
siho-song <thdtlgh234@naver.com>
2024-09-07 22:45:37 +0900

    [Refactor] 패키지 이동으로 인한 삭제

--- src/main/java/show/schedulemanagement/repository/schedule/detail/FScheduleDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package show.schedulemanagement.repository.schedule.detail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import show.schedulemanagement.domain.schedule.fSchedule.FScheduleDetail;
-
-public interface FScheduleDetailRepository extends JpaRepository<FScheduleDetail, Long> {
-}
