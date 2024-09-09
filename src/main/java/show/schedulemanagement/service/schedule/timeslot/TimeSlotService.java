81b8ee28c3eeda1fdd1a5f2dbe0395c1202e6418
siho-song <thdtlgh234@naver.com>
2024-09-09 23:26:13 +0900

    [Refactor] 코드 리팩토링

--- src/main/java/show/schedulemanagement/service/schedule/timeslot/TimeSlotService.java
+++ src/main/java/show/schedulemanagement/service/schedule/timeslot/TimeSlotService.java
@@ -6,10 +6,10 @@ import static show.schedulemanagement.constant.AppConstant.WORK_START_TIME;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -25,17 +25,17 @@ import show.schedulemanagement.domain.schedule.ScheduleAble;
 @Slf4j
 public class TimeSlotService {
 
-    public Map<LocalDate, List<TimeSlot>> findAllForPeriodByDate(
-            Map<LocalDate, List<ScheduleAble>> scheduleMap,
+    public Map<LocalDate, List<TimeSlot>> findAllBetweenStartAndEnd(
+            Map<LocalDate, List<ScheduleAble>> sortedScheduleAbleMap,
             LocalDate startDate,
             LocalDate endDate) {
 
         Map<LocalDate, List<TimeSlot>> slotsByDate = new HashMap<>();
-
         LocalDate currentDate = startDate;
+
         while (!currentDate.isAfter(endDate)) {
-            List<ScheduleAble> scheduleAbles = scheduleMap.getOrDefault(currentDate, Collections.emptyList());
-            List<TimeSlot> timeslots = findSlotsForDate(scheduleAbles, currentDate);
+            List<ScheduleAble> scheduleAbles = sortedScheduleAbleMap.getOrDefault(currentDate, Collections.emptyList());
+            List<TimeSlot> timeslots = findSlotsForDate(scheduleAbles);
             slotsByDate.put(currentDate, timeslots);
             currentDate = currentDate.plusDays(1);
         }
@@ -43,68 +43,23 @@ public class TimeSlotService {
         return slotsByDate;
     }
 
-    private List<TimeSlot> findSlotsForDate(List<ScheduleAble> scheduleAbles, LocalDate currentDate) {
-        List<TimeSlot> timeSlots = new ArrayList<>();
+    public Map<DayOfWeek, List<TimeSlot>> findCommonSlotsForEachDay(
+            Map<LocalDate, List<TimeSlot>> slotsForPeriodByDate,
+            Duration necessaryTime) {
 
-        if (scheduleAbles.isEmpty()) {
-            return addTotalWorkTime();
-        }
-
-        LocalDateTime dayStart = currentDate.atTime(WORK_START_TIME);
-        LocalDateTime dayEnd = currentDate.atTime(WORK_END_TIME);
-
-        // 일과 시작시간과 첫번째 스케쥴의 시작시간 사이시간을 확인하고, 0보다 클 경우 timeslot에 추가한다.
-        LocalDateTime firstScheduleStart = scheduleAbles.get(0).getStartDate();
-        if (canSlot(dayStart, firstScheduleStart)) {
-            timeSlots.add(new TimeSlot(dayStart.toLocalTime(), firstScheduleStart.toLocalTime()));
-        }
-
-        // 스케쥴끼리 비교하고 사이에 비는시간이 0보다 클 경우 timeslot에 추가한다.
-        LocalDateTime lastEndTime = null;
-        for (ScheduleAble scheduleAble : scheduleAbles) {
-            LocalDateTime startTime = scheduleAble.getStartDate();
-            LocalDateTime endTime = scheduleAble.getEndDate();
-
-            if (lastEndTime != null && canSlot(lastEndTime, startTime)) {
-                timeSlots.add(new TimeSlot(lastEndTime.toLocalTime(), startTime.toLocalTime()));
-            }
-            lastEndTime = endTime;
-        }
-
-        // 마지막 일정과 일과 종료시간의 사이시간을 확인하고, necassary 만큼의 여유가 있으면 timeslot에 추가한다.
-        LocalDateTime lastScheduleEnd = scheduleAbles.get(scheduleAbles.size() - 1).getEndDate();
-        if (canSlot(lastScheduleEnd, dayEnd)) {
-            timeSlots.add(new TimeSlot(lastScheduleEnd.toLocalTime(), dayEnd.toLocalTime()));
-        }
-
-        return timeSlots;
-    }
-
-    private boolean canSlot(LocalDateTime dayStart, LocalDateTime firstScheduleStart) {
-        return Duration.between(dayStart, firstScheduleStart).toMinutes() > 0;
-    }
-
-    private List<TimeSlot> addTotalWorkTime() {
-        return List.of(new TimeSlot(WORK_START_TIME, WORK_END_TIME));
-    }
-
-    public Map<DayOfWeek, List<TimeSlot>> findCommonRangeSlotsByDay(Map<LocalDate, List<TimeSlot>> allSlotsForPeriodByDate, Duration necessaryTime) {
         Map<DayOfWeek, List<TimeSlot>> commonRangeSlotsByDay = new EnumMap<>(DayOfWeek.class);
-        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
 
-            if(!hasAllDateSlots(allSlotsForPeriodByDate, dayOfWeek)){
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            if(!hasAllDateSlots(slotsForPeriodByDate, dayOfWeek)){
                 continue;
             }
 
-            List<List<TimeSlot>> allSlotsForDays = getSlotsByDay(allSlotsForPeriodByDate, dayOfWeek)
-                    .values().stream()
+            List<List<TimeSlot>> allSlotsForDateEqualDay = collectSlotsByDay(slotsForPeriodByDate, dayOfWeek)
+                    .values()
+                    .stream()
                     .toList();
 
-            if(allSlotsForDays.isEmpty()){
-                continue;
-            }
-
-            List<TimeSlot> commonTimeSlots = findCommonTimeSlots(necessaryTime, allSlotsForDays);
+            List<TimeSlot> commonTimeSlots = findCommonTimeSlots(necessaryTime, allSlotsForDateEqualDay);
 
             if(!commonTimeSlots.isEmpty()) {
                 commonRangeSlotsByDay.put(dayOfWeek, commonTimeSlots);
@@ -113,25 +68,85 @@ public class TimeSlotService {
         return commonRangeSlotsByDay;
     }
 
+    public Map<DayOfWeek, TimeSlot> selectRandomTimeSlots(List<DayOfWeek> days, Map<DayOfWeek, List<TimeSlot>> availableTimeSlotsByDay) {
+        return days.stream()
+                .filter(availableTimeSlotsByDay::containsKey)
+                .collect(Collectors.toMap(
+                        day -> day,
+                        day -> availableTimeSlotsByDay.get(day).get(0)
+                ));
+    }
+
+    private List<TimeSlot> findSlotsForDate(List<ScheduleAble> sortedScheduleAbles) {
+        List<TimeSlot> timeSlots = new ArrayList<>();
+
+        if (sortedScheduleAbles.isEmpty()) {
+            return addTotalWorkTime();
+        }
+
+        LocalTime firstScheduleStart = sortedScheduleAbles.get(0).getStartDate().toLocalTime();
+        if (isTimeSlotAvailable(WORK_START_TIME, firstScheduleStart)) {
+            timeSlots.add(new TimeSlot(WORK_START_TIME, firstScheduleStart));
+        }
+
+        LocalTime beforeScheduleEnd = null;
+        for (ScheduleAble scheduleAble : sortedScheduleAbles) {
+            LocalTime nextScheduleStart = scheduleAble.getStartDate().toLocalTime();
+            LocalTime nextScheduleEnd = scheduleAble.getEndDate().toLocalTime();
+
+            if (beforeScheduleEnd != null && isTimeSlotAvailable(beforeScheduleEnd, nextScheduleStart)) {
+                timeSlots.add(new TimeSlot(beforeScheduleEnd, nextScheduleStart));
+            }
+            beforeScheduleEnd = nextScheduleEnd;
+        }
+
+        LocalTime lastScheduleEnd = sortedScheduleAbles.get(sortedScheduleAbles.size() - 1).getEndDate().toLocalTime();
+        if (isTimeSlotAvailable(lastScheduleEnd, WORK_END_TIME)) {
+            timeSlots.add(new TimeSlot(lastScheduleEnd, WORK_END_TIME));
+        }
+        return timeSlots;
+    }
+
+    private List<TimeSlot> addTotalWorkTime() {
+        return List.of(new TimeSlot(WORK_START_TIME, WORK_END_TIME));
+    }
+
+    private boolean isTimeSlotAvailable(LocalTime start, LocalTime end) {
+        return Duration.between(start, end).toMinutes() > 0;
+    }
+
     private boolean hasAllDateSlots(Map<LocalDate, List<TimeSlot>> allSlotsForPeriodByDate, DayOfWeek dayOfWeek) {
         return allSlotsForPeriodByDate.entrySet().stream()
                 .filter(entry -> entry.getKey().getDayOfWeek().equals(dayOfWeek))
                 .noneMatch(entry -> entry.getValue().isEmpty());
     }
 
+    private LinkedHashMap<LocalDate, List<TimeSlot>> collectSlotsByDay(Map<LocalDate, List<TimeSlot>> slotsForPeriodByDate,
+                                                                       DayOfWeek dayOfWeek) {
+        return slotsForPeriodByDate.entrySet().stream()
+                .filter(entry -> entry.getKey().getDayOfWeek().equals(dayOfWeek))
+                .collect(Collectors.toMap(
+                        Entry::getKey,
+                        Entry::getValue,
+                        (existValue, newValue) -> existValue,
+                        LinkedHashMap::new
+                ));
+    }
+
     private List<TimeSlot> findCommonTimeSlots(Duration necessaryTime, List<List<TimeSlot>> allSlotsForDays) {
-        List<TimeSlot> finalList = new ArrayList<>(allSlotsForDays.get(0));
+        List<TimeSlot> finalList = new ArrayList<>(allSlotsForDays.get(0)); //최종리스트
+
         for (int i = 1; i < allSlotsForDays.size() ; i++) {
             List<TimeSlot> nextDaySlots = allSlotsForDays.get(i);
             List<TimeSlot> tempSlots = new ArrayList<>();
             for (TimeSlot slot1 : finalList) {
                 for (TimeSlot slot2 : nextDaySlots) {
-                    LocalTime start = max(slot1.getStartTime(), slot2.getStartTime());
-                    LocalTime end = min(slot1.getEndTime(), slot2.getEndTime());
+                    LocalTime start = getLaterTime(slot1.getStartTime(), slot2.getStartTime());
+                    LocalTime end = getEarlierTime(slot1.getEndTime(), slot2.getEndTime());
 
-                    Duration duration = Duration.between(start, end);
+                    Duration spareTime = Duration.between(start, end); //end-start
 
-                    if (!start.isAfter(end) && duration.compareTo(necessaryTime) >= 0) { // 겹치는 부분이 있는지 확인
+                    if (spareTime.compareTo(necessaryTime) >= 0) { // 겹치는 부분이 있는지 확인
                         tempSlots.add(new TimeSlot(start, end));
                     }
                 }
@@ -142,23 +157,11 @@ public class TimeSlotService {
         return finalList;
     }
 
-    private LinkedHashMap<LocalDate, List<TimeSlot>> getSlotsByDay(Map<LocalDate, List<TimeSlot>> allSlotsForPeriodByDate,
-                                                                DayOfWeek dayOfWeek) {
-        return allSlotsForPeriodByDate.entrySet().stream()
-                .filter(entry -> entry.getKey().getDayOfWeek().equals(dayOfWeek))
-                .collect(Collectors.toMap(
-                        Entry::getKey,
-                        Entry::getValue,
-                        (existValue, newValue) -> existValue,
-                        LinkedHashMap::new
-                ));
-    }
-
-    private LocalTime max(LocalTime time1, LocalTime time2) {
+    private LocalTime getLaterTime(LocalTime time1, LocalTime time2) {
         return time1.isAfter(time2) ? time1 : time2;
     }
 
-    private LocalTime min(LocalTime time1, LocalTime time2) {
+    private LocalTime getEarlierTime(LocalTime time1, LocalTime time2) {
         return time1.isBefore(time2) ? time1 : time2;
     }
 }
