1386619b0bc121151dfa78b21f2edf23b9038833
siho-song <thdtlgh234@naver.com>
2024-09-10 17:12:25 +0900

    [Feat] 일반일정 수정 엔드포인트구현
    + static import 추가 및 로그 제거

--- src/main/java/show/schedulemanagement/controller/schedule/NScheduleController.java
+++ src/main/java/show/schedulemanagement/controller/schedule/NScheduleController.java
@@ -1,20 +1,25 @@
 package show.schedulemanagement.controller.schedule;
 
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.*;
+
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.domain.schedule.nschedule.NSchedule;
 import show.schedulemanagement.dto.schedule.request.nschedule.NScheduleSave;
+import show.schedulemanagement.dto.schedule.request.nschedule.NScheduleUpdate;
 import show.schedulemanagement.dto.schedule.response.ScheduleResponse;
 import show.schedulemanagement.dto.Result;
+import show.schedulemanagement.dto.schedule.response.nschedule.NScheduleUpdateResponse;
 import show.schedulemanagement.service.MemberService;
 import show.schedulemanagement.service.schedule.nschedule.NScheduleService;
 import show.schedulemanagement.service.schedule.ScheduleService;
@@ -29,15 +34,22 @@ public class NScheduleController {
     private final MemberService memberService;
     private final NScheduleService nScheduleService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE , produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Result<ScheduleResponse>> addSchedule(@RequestBody @Valid NScheduleSave dto) throws RuntimeException{
+    @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Result<ScheduleResponse>> add(@RequestBody @Valid NScheduleSave dto) {
         Member member = memberService.getAuthenticatedMember();
-        NSchedule nSchedule = nScheduleService.addNSchedule(member, dto);
+        NSchedule nSchedule = nScheduleService.create(member, dto);
         scheduleService.save(nSchedule);
 
         Result<ScheduleResponse> result = new Result<>();
-        scheduleService.setResultFromSchedule(result,nSchedule);
-        log.debug("result : {} " , result);
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
+        scheduleService.setResultFromSchedule(result, nSchedule);
+        return new ResponseEntity<>(result, CREATED);
+    }
+
+    @PutMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<NScheduleUpdateResponse> update(@RequestBody @Valid NScheduleUpdate dto){
+        Member member = memberService.getAuthenticatedMember();
+        nScheduleService.update(member, dto);
+        NSchedule nSchedule = nScheduleService.findById(dto.getId());
+        return new ResponseEntity<>(NScheduleUpdateResponse.from(nSchedule), OK);
     }
 }
