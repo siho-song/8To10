e3facfbfbd6756cad624471abea7a79a1d7aa421
siho-song <thdtlgh234@naver.com>
2024-09-11 19:09:57 +0900

    [Feat] 일반 자식일정 부모일정과 함께 조회 메서드 구현

--- src/main/java/show/schedulemanagement/repository/schedule/nschedule/NScheduleDetailRepository.java
+++ src/main/java/show/schedulemanagement/repository/schedule/nschedule/NScheduleDetailRepository.java
@@ -1,7 +1,14 @@
 package show.schedulemanagement.repository.schedule.nschedule;
 
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import show.schedulemanagement.domain.schedule.nschedule.NScheduleDetail;
 
 public interface NScheduleDetailRepository extends JpaRepository<NScheduleDetail, Long> {
+    @EntityGraph(attributePaths = "nSchedule")
+    @Query("select nd from NScheduleDetail nd where nd.id = :id")
+    Optional<NScheduleDetail> findByIdWithParent(@Param(value = "id") Long id);
 }
