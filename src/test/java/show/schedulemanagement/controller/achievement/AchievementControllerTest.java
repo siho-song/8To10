e17cbb41fc342c9f0c37bdc355ba4130ab48cc1b
siho-song <thdtlgh234@naver.com>
2024-10-03 17:33:47 +0900

    [Test] 성취도 조회 엔드포인트 테스트 구현

---
+++ src/test/java/show/schedulemanagement/controller/achievement/AchievementControllerTest.java
@@ -0,0 +1,49 @@
+package show.schedulemanagement.controller.achievement;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockCookie;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import show.schedulemanagement.security.dto.LoginMemberDto;
+import show.schedulemanagement.security.utils.TokenUtils;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("성취도 컨트롤러 테스트")
+@Transactional
+class AchievementControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    TokenUtils tokenUtils;
+
+    String token;
+    MockCookie jwtCookie;
+
+    @BeforeEach
+    void init(){
+        token = tokenUtils.generateJwtToken(new LoginMemberDto("normal@example.com")); // 토큰 생성
+        jwtCookie = new MockCookie("jwt", token); // JWT 쿠키 생성
+    }
+
+    @Test
+    @DisplayName("유저의 월간 성취도 조회")
+    void month_achievement_search() throws Exception {
+        int year = 2024;
+        int month  = 1;
+        mockMvc.perform(get("/achievement/{year}/{month}", year, month)
+                        .cookie(jwtCookie)
+                ).andExpect(status().isOk());
+    }
+}
