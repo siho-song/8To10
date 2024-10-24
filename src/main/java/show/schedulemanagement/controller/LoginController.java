53eac8927be83205be245918f525a950b7aff88f
siho-song <thdtlgh234@naver.com>
2024-10-24 19:09:34 +0900

    [Refactor] 미사용 메서드 제거

--- src/main/java/show/schedulemanagement/controller/LoginController.java
+++ src/main/java/show/schedulemanagement/controller/LoginController.java
@@ -1,34 +1,18 @@
 package show.schedulemanagement.controller;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import show.schedulemanagement.service.MemberService;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
-    private final MemberService memberService;
-
     @GetMapping("/")
     public String loginView() {
-        return "login/login.html";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpServletResponse response) {
-        // JWT 토큰을 저장하는 쿠키의 값을 삭제
-        Cookie jwtCookie = new Cookie("jwt", null);
-        jwtCookie.setMaxAge(0);  // 쿠키의 유효기간을 0으로 설정하여 즉시 삭제
-        jwtCookie.setPath("/");
-        response.addCookie(jwtCookie);
-
-        return "redirect:/";
+        return "login/login";
     }
 
     @PostMapping("/login")
