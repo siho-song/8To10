0aaacc3d34a5dae8309a7fde767e0ddb4aa4dd73
siho-song <thdtlgh234@naver.com>
2024-10-31 22:07:55 +0900

    [Feat] 파라미터로 객체대신 id를 받도록 변경

--- src/main/java/show/schedulemanagement/service/board/reply/ReplyHeartService.java
+++ src/main/java/show/schedulemanagement/service/board/reply/ReplyHeartService.java
@@ -6,10 +6,8 @@ import show.schedulemanagement.domain.board.reply.ReplyHeart;
 import show.schedulemanagement.domain.member.Member;
 
 public interface ReplyHeartService {
-    void deleteByReply(Reply reply);
-    void deleteByReplies(List<Reply> replies);
-    boolean existsReplyHeartByMemberAndReply(Member member, Reply reply);
-    ReplyHeart findByMemberAndReply(Member member, Reply reply);
-    void add(Reply reply, Member member);
-    void delete(Reply reply, Member member);
+    boolean existsReplyHeartByMemberAndReplyId(Member member, Long replyId);
+    ReplyHeart findByMemberAndReplyId(Member member, Long replyId);
+    void add(Long replyId, Member member);
+    void delete(Long replyId, Member member);
 }
