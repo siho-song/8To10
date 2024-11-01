de840e2941b06d5e1ba4542891080a9976262466
siho-song <thdtlgh234@naver.com>
2024-11-01 16:46:45 +0900

    [Feat] Role 필드 추가

--- src/main/java/show/schedulemanagement/domain/member/Role.java
+++ src/main/java/show/schedulemanagement/domain/member/Role.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum Role {
 
-    ADMIN("관리자"),NORMAL_USER("일반 유저"),PUNCTUAL_USER("일정을 잘 지키는 유저");
+    ADMIN("관리자"),NORMAL_USER("일반 유저"), FAITHFUL_USER("일정을 잘 지키는 유저");
 
     private final String role;
 
