95463dd6ebeead134dff56d8b800f0124ce1eb0e
siho-song <thdtlgh234@naver.com>
2024-11-01 16:49:25 +0900

    [Feat] Auth 엔티티 구현

--- src/main/java/show/schedulemanagement/domain/auth/Auth.java
+++ src/main/java/show/schedulemanagement/domain/auth/Auth.java
@@ -1,13 +1,11 @@
-package show.schedulemanagement.domain.member;
+package show.schedulemanagement.domain.auth;
 
-import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -18,8 +16,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.ColumnDefault;
 import show.schedulemanagement.domain.auditing.baseentity.BaseEntity;
+import show.schedulemanagement.domain.member.Member;
 
 @Entity
 @Getter
@@ -27,34 +25,17 @@ import show.schedulemanagement.domain.auditing.baseentity.BaseEntity;
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 @ToString(exclude = {"member"})
-public class MemberRole extends BaseEntity {
+public class Auth extends BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "member_role_id")
+    @Column(name = "auth_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
-    @Enumerated(value = STRING)
-    @Column(nullable = false)
-    @ColumnDefault(value = "NORMAL_USER")
-    private Role role;
-
-    private void setRole(Member member, Role role){
-        this.role= role;
-        member.getMemberRoles().add(this);
-    }
-
-    public static MemberRole createMemberRoleInfo(Member member, Role role){
-        MemberRole memberRole = MemberRole.builder()
-                .member(member)
-                .build();
-
-        memberRole.setRole(member,role);
-        return memberRole;
-    }
+    private String refreshToken;
 
     @PrePersist
     void prePersist(){
