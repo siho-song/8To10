d94804f18d640bb84bfba9cfbaac5f3fd5decceb
siho-song <thdtlgh234@naver.com>
2024-11-03 21:37:55 +0900

    [Feat] 토큰 갱신 엔드포인트 구현

---
+++ src/main/java/show/schedulemanagement/controller/AuthController.java
@@ -0,0 +1,32 @@
+package show.schedulemanagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import show.schedulemanagement.dto.auth.AccessTokenResponse;
+import show.schedulemanagement.service.auth.AuthService;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping("/")
+    public String loginView() {
+        return "login/login";
+    }
+
+    @GetMapping(value = "/renew")
+    public ResponseEntity<AccessTokenResponse> renewAccessToken(
+            @CookieValue(name = "refresh_token") String refreshToken,
+            @RequestHeader("Authorization") String accessTokenHeader)
+    {
+
+        String renewAccessToken = authService.getRenewalAccessToken(refreshToken, accessTokenHeader);
+        return ResponseEntity.ok(AccessTokenResponse.from(renewAccessToken));
+    }
+}
