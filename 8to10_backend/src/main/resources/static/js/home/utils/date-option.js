a57325fb6c502affd2901644365dd254a5b76bfb
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-05 18:16:21 +0900

    [Feat] 백엔드 디렉터리 하위 프론트엔드 코드 삭제

--- 8to10_backend/src/main/resources/static/js/home/utils/date-option.js
+++
@@ -1,5 +0,0 @@
-function setDefaultDate() {
-    var today = new Date().toISOString().split('T')[0];
-    document.getElementById('schedule-start-date').value = today;
-    document.getElementById('schedule-end-date').value = today;
-}
