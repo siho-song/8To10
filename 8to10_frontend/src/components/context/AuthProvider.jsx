7c225b7a14057c6a264a0b92948e992ecfdfd6ab
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-07 15:43:40 +0900

    [Feat] Access Token 로컬 스토리지에 저장
    - 로그인 요청(loginUser)을 날리는 메소드에서 response의 헤더에서 Access Token을 꺼냄
    - 꺼낸 Access Token을 localStorage에 저장

--- 8to10_frontend/src/components/context/AuthProvider.jsx
+++ 8to10_frontend/src/components/context/AuthProvider.jsx
@@ -15,22 +15,33 @@ function AuthProvider({ children }) {
         try {
             const response = (await fetch(`/api/login`, {
                 method: 'POST',
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
                 body: paramString.toString(),
             }));
-            console.log("response : ", response.ok);
-            if (response.ok) {
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                setIsAuthenticated(false);
+                setErrorMessage(errorData.message + ' => 로그인에 실패했습니다.');
+                throw new Error(errorMessage);
+            }
+
+            const accessToken = response.headers.get('authorization').split(' ')[1];
+            console.log(accessToken);
+            if (accessToken) {
+                localStorage.setItem('authorization', accessToken);
                 setIsAuthenticated(true);
                 setErrorMessage('');
                 setEmail(email);
             } else {
-                const errorData = await response.json();
+                throw new Error("액세스 토큰을 확인할 수 없습니다.");
                 setIsAuthenticated(false);
                 setErrorMessage(errorData.message || '로그인 실패');
             }
         } catch (error) {
-            console.error("Error : ", error);
+            console.error(error);
             setIsAuthenticated(false);
             setErrorMessage( '로그인 실패');
         } finally {
