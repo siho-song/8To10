57fbd48efcd64be1825bcc3d0a6e3e823126950a
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-07 15:46:03 +0900

    [Feat] 불필요한 주석, 코드 제거

--- 8to10_frontend/src/components/home/HomeLeftSideBar.jsx
+++ 8to10_frontend/src/components/home/HomeLeftSideBar.jsx
@@ -3,7 +3,6 @@ import "@/styles/home/LeftSideBar..css"
 function HomeSidebarLeft() {
     return (
         <div className="sidebar" id="left-sidebar">
-            <p>왼쪽 사이드바</p>
             <div className="achievement-box">
                 <span>User</span> 님의 <br /> 달성률은 <strong>(95.5%)</strong> 입니다.
                 <button className="details-button" onClick={() => window.location.href = '/achievement'}>
