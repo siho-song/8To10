a407dc9717ef65a3cf787cb6ad63dc360fb579ad
siho-song <thdtlgh234@naver.com>
2024-11-08 15:41:25 +0900

    [Feat] 커스텀 예외 적용

--- 8to10_backend/src/main/java/show/schedulemanagement/service/auth/MemberDetailsService.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/service/auth/MemberDetailsService.java
@@ -6,25 +6,23 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.domain.auth.MemberDetails;
-import show.schedulemanagement.service.MemberService;
+import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.exception.ExceptionCode;
+import show.schedulemanagement.exception.UserAuthenticationException;
+import show.schedulemanagement.repository.member.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class MemberDetailsService implements UserDetailsService {
 
-    private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Member member = memberService.findByEmail(email);
-
-        log.debug("MemberDetailsService call loadUserByUsername : {}",member);
-        if (member == null) {
-            throw new UsernameNotFoundException("No member found with email: " + email);
-        }
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new UserAuthenticationException(ExceptionCode.INVALID_EMAIL));
         return new MemberDetails(member);
     }
 }
