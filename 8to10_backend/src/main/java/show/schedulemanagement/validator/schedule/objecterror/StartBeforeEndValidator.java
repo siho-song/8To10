213981ffd44127727b47f78f0698d9819fe4eaed
siho-song <thdtlgh234@naver.com>
2024-11-08 16:44:36 +0900

    [Feat] null체크 로직 제거

--- 8to10_backend/src/main/java/show/schedulemanagement/validator/schedule/objecterror/StartBeforeEndValidator.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/validator/schedule/objecterror/StartBeforeEndValidator.java
@@ -2,15 +2,13 @@ package show.schedulemanagement.validator.schedule.objecterror;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.time.LocalDateTime;
 import show.schedulemanagement.dto.schedule.request.DateRangeValidatable;
 
 public class StartBeforeEndValidator implements ConstraintValidator<StartBeforeEnd, DateRangeValidatable> {
 
     @Override
     public boolean isValid(DateRangeValidatable value, ConstraintValidatorContext context) {
-        if(value == null){
-            return true;
-        }
         return value.takeStartDateTime().isBefore(value.takeEndDateTime());
     }
 }
