270bd567f419066385c1c3a0ba4a91a3e0a25cd9
siho-song <thdtlgh234@naver.com>
2024-11-08 17:49:26 +0900

    [Test] 예외 변경 및 테스트 로직 변경

--- 8to10_backend/src/test/java/show/schedulemanagement/service/auth/AuthServiceTest.java
+++ 8to10_backend/src/test/java/show/schedulemanagement/service/auth/AuthServiceTest.java
@@ -1,12 +1,14 @@
 package show.schedulemanagement.service.auth;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import io.jsonwebtoken.JwtException;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +18,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import show.schedulemanagement.domain.auth.Auth;
+import show.schedulemanagement.exception.InvalidTokenException;
 import show.schedulemanagement.provider.TokenProvider;
 import show.schedulemanagement.repository.auth.AuthRepository;
 import show.schedulemanagement.utils.BearerAuthorizationUtils;
@@ -47,7 +50,7 @@ class AuthServiceTest {
     @DisplayName("Refresh 토큰이 유효하고 Access 토큰이 만료 되었을 때 새로운 AccessToken 을 반환한다.")
     void getRenewalAccessToken() {
         //given
-        when(tokenProvider.isValidToken(any())).thenReturn(true);
+        doNothing().when(tokenProvider).validateRefreshToken(any());
         when(tokenProvider.isExpiredToken(any())).thenReturn(true);
 
         Auth auth = Auth.from("normal@example.com", null);
@@ -58,7 +61,7 @@ class AuthServiceTest {
         String renewalAccessToken = authService.getRenewalAccessToken(refreshToken, accessToken);
 
         //then
-        verify(tokenProvider, times(1)).isValidToken(refreshToken);
+        verify(tokenProvider, times(1)).validateRefreshToken(refreshToken);
         verify(tokenProvider, times(1)).isExpiredToken(accessToken);
         verify(authRepository, times(1)).findByRefreshToken(refreshToken);
         assertThat(renewalAccessToken).isEqualTo("New AccessToken");
@@ -68,43 +71,41 @@ class AuthServiceTest {
     @DisplayName("RefreshToken이 유효하지 않은 경우 예외가 발생한다.")
     void inValidRefreshToken() {
         //given
-        when(tokenProvider.isValidToken(refreshToken)).thenReturn(false);
+        doThrow(InvalidTokenException.class).when(tokenProvider).validateRefreshToken(refreshToken);
 
         //when,then
         assertThatThrownBy(() -> authService.getRenewalAccessToken(refreshToken, accessTokenHeader))
-                .isInstanceOf(JwtException.class);
-        verify(tokenProvider,times(1)).isValidToken(refreshToken);
+                .isInstanceOf(InvalidTokenException.class);
+        verify(tokenProvider,times(1)).validateRefreshToken(refreshToken);
     }
 
     @Test
     @DisplayName("AccessToken이 유효하지 않은 경우 예외가 발생한다.")
     void inValidAccessToken() {
         //given
-        when(tokenProvider.isValidToken(refreshToken)).thenReturn(true);
-        when(tokenProvider.isValidToken(accessToken)).thenReturn(false);
+        doNothing().when(tokenProvider).validateRefreshToken(any());
+        when(tokenProvider.isExpiredToken(accessToken)).thenReturn(false);
 
         //when,then
         assertThatThrownBy(() -> authService.getRenewalAccessToken(refreshToken, accessToken))
-                .isInstanceOf(JwtException.class);
-        verify(tokenProvider, times(1)).isValidToken(refreshToken);
-        verify(tokenProvider, times(1)).isValidToken(accessToken);
+                .isInstanceOf(InvalidTokenException.class);
+        verify(tokenProvider, times(1)).validateRefreshToken(refreshToken);
+        verify(tokenProvider, times(1)).isExpiredToken(accessToken);
     }
 
     @Test
     @DisplayName("RefreshToken이 유효하고 ,만료되지 않은 AccessToken인 경우 기존의 AccessToken을 반환한다.")
     void validAccessToken() {
         //given
-        when(tokenProvider.isValidToken(refreshToken)).thenReturn(true);
-        when(tokenProvider.isExpiredToken(accessToken)).thenReturn(false);
+        doNothing().when(tokenProvider).validateRefreshToken(any());
         when(tokenProvider.isValidToken(accessToken)).thenReturn(true);
 
         //when
         String renewalAccessToken = authService.getRenewalAccessToken(refreshToken, accessToken);
 
         //then
-        assertThat(renewalAccessToken).isEqualTo("AccessToken");
-        verify(tokenProvider,times(1)).isValidToken(refreshToken);
-        verify(tokenProvider,times(1)).isExpiredToken(accessToken);
+        assertThat(renewalAccessToken).isEqualTo(accessToken);
+        verify(tokenProvider,times(1)).validateRefreshToken(refreshToken);
         verify(tokenProvider, times(1)).isValidToken(accessToken);
     }
 }
