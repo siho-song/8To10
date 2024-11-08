75e7af50596ae67f65eb11137ce4fd93b5b58b51
siho-song <thdtlgh234@naver.com>
2024-11-08 16:32:54 +0900

    [Refactor] 미사용 클래스 제거

--- 8to10_backend/src/main/java/show/schedulemanagement/service/schedule/ScheduleDetailService.java
+++
@@ -1,9 +0,0 @@
-package show.schedulemanagement.service.schedule;
-
-import show.schedulemanagement.domain.member.Member;
-import show.schedulemanagement.domain.schedule.nschedule.NScheduleDetail;
-
-public interface ScheduleDetailService {
-    void deleteNdById(Member member, Long id);
-    NScheduleDetail findNdById(Long id);
-}
