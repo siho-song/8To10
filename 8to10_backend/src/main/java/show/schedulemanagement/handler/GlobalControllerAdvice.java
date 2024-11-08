7ee755a78fc6c531e3d7e0796feb2e9fbd9f8820
siho-song <thdtlgh234@naver.com>
2024-11-08 16:31:28 +0900

    [Feat] ControllerAdvice 구현

---
+++ 8to10_backend/src/main/java/show/schedulemanagement/handler/GlobalControllerAdvice.java
@@ -0,0 +1,91 @@
+package show.schedulemanagement.handler;
+
+import static show.schedulemanagement.exception.ExceptionCode.INTERNAL_SERVER_ERROR;
+import static show.schedulemanagement.exception.ExceptionCode.INVALID_REQUEST;
+
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestCookieException;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import show.schedulemanagement.dto.ErrorResponse;
+import show.schedulemanagement.exception.AuthException;
+import show.schedulemanagement.exception.BusinessException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request)
+    {
+        String errorMessage = createErrorMessage(ex.getBindingResult());
+        log.warn(errorMessage, ex);
+
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(INVALID_REQUEST.getCode(), errorMessage));
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorResponse> handleMissingHeaderException(MissingRequestHeaderException e){
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(INVALID_REQUEST.getCode(), "누락된 헤더 : " + e.getHeaderName()));
+    }
+
+    @ExceptionHandler(MissingRequestCookieException.class)
+    public ResponseEntity<ErrorResponse> handleMissingCookieException(MissingRequestCookieException e){
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(INVALID_REQUEST.getCode(), "누락된 쿠키 : " + e.getCookieName()));
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e){
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorResponse> handleAuthException(AuthException e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.internalServerError()
+                .body(ErrorResponse.of(INTERNAL_SERVER_ERROR.getCode(), INTERNAL_SERVER_ERROR.getMessage()));
+    }
+
+    private String createErrorMessage(BindingResult bindingResult) {
+        StringBuilder sb = new StringBuilder();
+
+        bindingResult.getFieldErrors().forEach(fieldError ->
+                sb.append(fieldError.getField())
+                  .append(" 필드 검증 오류 : ")
+                  .append(fieldError.getDefaultMessage())
+                  .append(System.lineSeparator())
+        );
+
+        bindingResult.getGlobalErrors().forEach(globalError ->
+                sb.append(" 객체 검증 오류 : ")
+                  .append(globalError.getDefaultMessage())
+                  .append(System.lineSeparator())
+        );
+
+        return sb.toString();
+    }
+}
