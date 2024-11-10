d6eb46dc03d7ba24167af202d152e70f041d6156
siho-song <thdtlgh234@naver.com>
2024-11-10 20:27:42 +0900

    [Feat] 유저 스탯 ResponseDto 구현

---
+++ 8to10_backend/src/main/java/show/schedulemanagement/dto/UserStatsResponse.java
@@ -0,0 +1,24 @@
+package show.schedulemanagement.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import show.schedulemanagement.domain.member.Role;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+public class UserStatsResponse {
+    private String nickname;
+    private String role;
+    private double achievementRate;
+
+    public static UserStatsResponse of(String nickname, String role, double achievementRate) {
+        UserStatsResponse statsResponse = new UserStatsResponse();
+        statsResponse.nickname = nickname;
+        statsResponse.role= role;
+        statsResponse.achievementRate = achievementRate;
+        return statsResponse;
+    }
+}
