58f7d34f2c14b71589aa62a2f4d0ce3e5bfcaa02
siho-song <thdtlgh234@naver.com>
2024-11-10 20:31:16 +0900

    [Test] 유저 스텟 반환 컨트롤러 테스트 구현

---
+++ 8to10_backend/src/test/java/show/schedulemanagement/controller/HomeControllerTest.java
@@ -0,0 +1,53 @@
+package show.schedulemanagement.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+import show.schedulemanagement.provider.TokenProvider;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    TokenProvider tokenProvider;
+
+    String token;
+
+    @BeforeEach
+    void init(){
+        token = tokenProvider.generateAccessToken("normal@example.com"); // 토큰 생성
+    }
+
+    @Test
+    @DisplayName("유저 스텟 정보를 반환한다.")
+    @Transactional
+    void getUserStat() throws Exception {
+        ResultActions result = mockMvc.perform(get("/home/user-stats")
+                .header("Authorization", "Bearer " + token)
+                .accept(APPLICATION_JSON)
+        );
+
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.nickname").exists())
+                .andExpect(jsonPath("$.nickname").isNotEmpty())
+                .andExpect(jsonPath("$.role").exists())
+                .andExpect(jsonPath("$.role").isNotEmpty())
+                .andExpect(jsonPath("$.achievementRate").exists())
+                .andExpect(jsonPath("$.achievementRate").isNotEmpty());
+    }
+}
