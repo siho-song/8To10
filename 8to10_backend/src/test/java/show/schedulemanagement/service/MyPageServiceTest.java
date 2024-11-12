605a9fd67da6304ebec2abbc4d237a4444585a0c
siho-song <thdtlgh234@naver.com>
2024-11-12 19:00:48 +0900

    [Test] 유저가 쓴 댓글 조회 메서드 테스트 구현

--- 8to10_backend/src/test/java/show/schedulemanagement/service/MyPageServiceTest.java
+++ 8to10_backend/src/test/java/show/schedulemanagement/service/MyPageServiceTest.java
@@ -1,5 +1,6 @@
 package show.schedulemanagement.service;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -11,11 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 import show.schedulemanagement.domain.board.Board;
+import show.schedulemanagement.domain.board.reply.Reply;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.Result;
 import show.schedulemanagement.dto.mypage.MemberBoardsResponse;
+import show.schedulemanagement.dto.mypage.MemberRepliesResponse;
 import show.schedulemanagement.dto.mypage.ProfileResponse;
 import show.schedulemanagement.service.board.BoardService;
+import show.schedulemanagement.service.board.reply.ReplyService;
 
 @SpringBootTest
 @DisplayName("마이페이지 서비스 테스트")
@@ -23,6 +27,9 @@ class MyPageServiceTest {
 
     @MockBean
     BoardService boardService;
+
+    @MockBean
+    ReplyService replyService;
 
     @Autowired
     MyPageService myPageService;
@@ -64,5 +71,27 @@ class MyPageServiceTest {
 
         //then
         assertThat(items).hasSize(boards.size());
+    }
+
+    @Test
+    @DisplayName("유저가 작성한 댓글을 불러온다.")
+    @WithUserDetails("normal@example.com")
+    void getMemberReplies() {
+        //given
+        Member member = memberService.getAuthenticatedMember();
+        Board board1 = Board.builder().id(1L).build();
+        Board board2 = Board.builder().id(2L).build();
+        Reply reply1 = Reply.builder().member(member).board(board1).contents("test1").build();
+        Reply reply2 = Reply.builder().member(member).board(board2).contents("test2").build();
+        List<Reply> replies = List.of(reply1, reply2);
+
+        when(replyService.findAllByMemberWithBoard(member)).thenReturn(replies);
+
+        //when
+        Result<MemberRepliesResponse> result = myPageService.getMemberReplies(member);
+        List<MemberRepliesResponse> items = result.getItems();
+
+        //then
+        assertThat(items).hasSize(replies.size());
     }
 }
