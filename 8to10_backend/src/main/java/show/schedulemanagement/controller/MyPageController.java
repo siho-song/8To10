b04d5bb099ed52d5dcd4fbb979185764fb8df815
siho-song <thdtlgh234@naver.com>
2024-11-12 17:23:30 +0900

    [Feat] 마이페이지 프로필 불러오기 엔드포인트 구현

---
+++ 8to10_backend/src/main/java/show/schedulemanagement/controller/MyPageController.java
@@ -0,0 +1,25 @@
+package show.schedulemanagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import show.schedulemanagement.config.web.CurrentMember;
+import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.dto.mypage.ProfileResponse;
+import show.schedulemanagement.service.MyPageService;
+
+@RestController
+@RequestMapping("/mypage")
+@RequiredArgsConstructor
+public class MyPageController {
+
+    private final MyPageService myPageService;
+
+    @GetMapping
+    public ResponseEntity<ProfileResponse> getProfile(@CurrentMember Member member) {
+        return ResponseEntity.ok(myPageService.getProfile(member));
+    }
+
+}
