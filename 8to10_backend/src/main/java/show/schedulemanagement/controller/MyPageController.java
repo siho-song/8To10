a81d49a434dfad897b35a6d9694c90c739f0ec87
siho-song <thdtlgh234@naver.com>
2024-11-12 20:25:04 +0900

    [Feat] 닉네임 업데이트 엔드포인트 구현

--- 8to10_backend/src/main/java/show/schedulemanagement/controller/MyPageController.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/controller/MyPageController.java
@@ -1,15 +1,20 @@
 package show.schedulemanagement.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import show.schedulemanagement.config.web.CurrentMember;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.dto.Result;
 import show.schedulemanagement.dto.mypage.MemberBoardsResponse;
 import show.schedulemanagement.dto.mypage.MemberRepliesResponse;
+import show.schedulemanagement.dto.mypage.NicknameUpdateRequest;
 import show.schedulemanagement.dto.mypage.ProfileResponse;
 import show.schedulemanagement.dto.mypage.ScrappedBoardResponse;
 import show.schedulemanagement.service.MyPageService;
@@ -39,5 +44,14 @@ public class MyPageController {
     @GetMapping("/scrapped-boards")
     public ResponseEntity<Result<ScrappedBoardResponse>> getScrapBoards(@CurrentMember Member member) {
         return ResponseEntity.ok(myPageService.getScrappedBoard(member));
+    }
+
+    @PutMapping("/account/nickname")
+    public ResponseEntity<Void> updateNickname(
+            @RequestBody @Valid NicknameUpdateRequest dto,
+            @CurrentMember Member member)
+    {
+        myPageService.updateNickname(dto.getNickname(), member.getId());
+        return ResponseEntity.noContent().build();
     }
 }
