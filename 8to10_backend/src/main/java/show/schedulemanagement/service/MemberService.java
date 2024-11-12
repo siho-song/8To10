acb5f2128f884c823b65af0c87a043b85e582957
siho-song <thdtlgh234@naver.com>
2024-11-12 19:28:45 +0900

    [Feat] id로 단건조회 메서드 구현

--- 8to10_backend/src/main/java/show/schedulemanagement/service/MemberService.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/service/MemberService.java
@@ -3,6 +3,7 @@ package show.schedulemanagement.service;
 import show.schedulemanagement.domain.member.Member;
 
 public interface MemberService {
+    Member findById(Long id);
     Member findByEmail(String email);
     Member getAuthenticatedMember();
 }
