2534fa79be0f3f94c0dd15a9cc1e74c8c900b721
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-20 01:53:44 +0900

    [Feat] "/home/user-stats"로 GET 요청하는 메소드 구현 (loadUserStats)
    - get 요청
    - ApiEndPoints.js에 상수로 등록

--- 8to10_frontend/src/components/home/userStatsCard/UserStatsCard.jsx
+++ 8to10_frontend/src/components/home/userStatsCard/UserStatsCard.jsx
@@ -1,9 +1,37 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import authenticatedApi from "@/api/AuthenticatedApi.js";
+import {API_ENDPOINT_NAMES} from "@/constants/ApiEndPoints.js";
 
 
 const UserStatsCard = () => {
     const [nickname, setNickname] = useState('unknown');
+    const [role, setRole] = useState('');
     const [achievementRate, setAchievementRate] = useState(0);
+
+    useEffect(() => {
+        const loadUserStats = async () => {
+            try {
+                const url = '/home/user-stats';
+                const response = await authenticatedApi.get(
+                    url,
+                    {
+                        apiEndPoint: API_ENDPOINT_NAMES.GET_USER_STATSCARD,
+                    }
+                );
+
+                const data = response.data;
+
+                setNickname(data.nickname);
+                setRole(data.role);
+                setAchievementRate(data.achievementRate);
+            } catch(error) {
+                console.error(error.toString());
+                console.error(error);
+            }
+        };
+
+        loadUserStats();
+    },[]);
 
     return (
         <div className="achievement-box">
