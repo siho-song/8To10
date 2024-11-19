5bfcf0c428fb3688ab1ba79a50fda08d9562b4f1
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-19 22:31:06 +0900

    [Feat] Private Route 에 액세스 토큰 리프레시 추가
    - 새로고침을 하거나 브라우저의 주소창을 통해 접근하는 경우 브라우저에 저장된 액세스 토큰이 있는지 확인하고, 있으면 인증상태를 유효한 것으로 처리해주는 함수 구현

--- 8to10_frontend/src/components/PrivateRoute.jsx
+++ 8to10_frontend/src/components/PrivateRoute.jsx
@@ -1,24 +1,37 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import {useAuth} from "@/context/UseAuth.jsx";
 import {useEffect} from "react";
+import {refreshAccessToken} from "@/helpers/TokenUtils.js";
 
 function PrivateRoute() {
-    const {isAuthenticated, setIsAuthenticated, loading, setLoading, setEmail, errorMessage} = useAuth();
+    const {isAuthenticated, setIsAuthenticated, loading, setLoading, setEmail} = useAuth();
 
     useEffect(() => {
-        if (!isAuthenticated && !errorMessage) {
-            const accessToken = localStorage.getItem('Authorization');
-            const email = localStorage.getItem('Email');
+        const refreshAuthentication = async () => {
+            if (!isAuthenticated) {
+                try {
+                    const accessToken = localStorage.getItem('Authorization');
+                    const email = localStorage.getItem('Email');
 
-            if (accessToken && email) {
-                setIsAuthenticated(true);
-                setEmail(email);
-            } else {
-                setIsAuthenticated(false);
-                localStorage.clear();
+                    if (accessToken && email) {
+                        const response = await refreshAccessToken();
+                        if (response) {
+                            setIsAuthenticated(true);
+                            setEmail(email);
+                        }
+                    }
+                } catch(error) {
+                    console.error(error.toString());
+                    console.error(error);
+                } finally {
+                    setLoading(false);
+                }
             }
-            setLoading(false);
         }
+        if (!isAuthenticated) {
+            setLoading(true);
+        }
+        refreshAuthentication();
     }, [isAuthenticated]);
 
     if (loading) {
