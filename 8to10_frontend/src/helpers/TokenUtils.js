6eba1c2ba7dc42a6c29bb3946b5539df2bd11612
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-19 22:13:38 +0900

    [Feat] TokenUtils.js안의 토큰 유틸 함수명 수정 및 refreshAccessToken 함수 추가
    + 디렉터리 이동 (helpers)

---
+++ 8to10_frontend/src/helpers/TokenUtils.js
@@ -0,0 +1,28 @@
+import authenticatedApi from "@/api/AuthenticatedApi.js";
+import {API_ENDPOINT_NAMES} from "@/constants/ApiEndPoints.js";
+
+export const parseBearerToken = (token) => {
+    if (!token) return null;
+
+    const bearerRegex = /^Bearer\s+(.+)$/;
+    const match = token.match(bearerRegex);
+    return match ? match[1] : null;
+};
+
+export const buildBearerToken = (token) => {
+    if (!token) return null;
+    return 'Bearer ' + token;
+};
+
+
+export const refreshAccessToken = async() => {
+    try {
+        const response = await authenticatedApi.post(
+            '/renew',
+            {},
+            {apiEndPoint: API_ENDPOINT_NAMES.RENEW});
+        return response.data['accessToken'];
+    } catch (error) {
+        console.error(error)
+    }
+}
