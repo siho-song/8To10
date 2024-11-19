8a638df7fee8b700bf989f3499ef8a70ef52e2a0
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-20 01:37:51 +0900

    [Feat] 사용자 닉네임과 사용자의 성취도 비율의 상태를 useState로 지정

--- 8to10_frontend/src/components/home/userStatsCard/UserStatsCard.jsx
+++ 8to10_frontend/src/components/home/userStatsCard/UserStatsCard.jsx
@@ -1,10 +1,13 @@
+import {useState} from "react";
 
 
 const UserStatsCard = () => {
+    const [nickname, setNickname] = useState('unknown');
+    const [achievementRate, setAchievementRate] = useState(0);
 
     return (
         <div className="achievement-box">
-            <span>User</span> 님의 <br/> 달성률은 <strong>(95.5%)</strong> 입니다.
+            <span>{nickname}</span> 님의 <br/> 달성률은 <strong>({achievementRate}%)</strong> 입니다.
             <button className="details-button" onClick={() => window.location.href = '/achievement'}>
                 자세히 보기
             </button>
