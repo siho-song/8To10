6c235361383ab1107e754bb71365b7101aee0003
siho-song <thdtlgh234@naver.com>
2024-11-20 19:01:26 +0900

    [Refactor] 상속 객체 변경
    - 필터의 AuthenticationException 처리 핸들러 사용과 서비스 레이어에서의 인증관련 에러 통합

--- 8to10_backend/src/main/java/show/schedulemanagement/exception/AuthException.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/exception/AuthException.java
@@ -1,13 +1,15 @@
 package show.schedulemanagement.exception;
 
 import lombok.Getter;
+import org.springframework.security.core.AuthenticationException;
 
 @Getter
-public class AuthException extends RuntimeException{
+public class AuthException extends AuthenticationException {
     private final int code;
     private final String message;
 
     public AuthException(ExceptionCode exceptionCode) {
+        super(exceptionCode.getMessage());
         this.code = exceptionCode.getCode();
         this.message = exceptionCode.getMessage();
     }
