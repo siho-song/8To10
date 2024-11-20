a30d01c98c04f37a09637fbdb2e4cb5619d31449
siho-song <thdtlgh234@naver.com>
2024-11-20 19:03:41 +0900

    [Feat] baseTargetUrl 필드 추가

--- 8to10_backend/src/main/java/show/schedulemanagement/domain/notification/NotificationType.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/domain/notification/NotificationType.java
@@ -4,13 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum NotificationType {
-    REPLY_ADD("댓글 추가"),
-    TODO_UPDATE("TODO 업데이트 촉구"),
-    ACHIEVEMENT_FEEDBACK("성취도 피드백");
+    REPLY_ADD("댓글 추가", "/board"),
+    NESTED_REPLY_ADD("대댓글 추가", "/board"),
+    TODO_UPDATE("TODO 업데이트", null),
+    ACHIEVEMENT_FEEDBACK("성취도", null),
+    ;
 
     private final String value;
+    private final String baseTargetUrl;
 
-    NotificationType(String value) {
+    NotificationType(String value, String baseTargetUrl) {
         this.value = value;
+        this.baseTargetUrl = baseTargetUrl;
     }
 }
