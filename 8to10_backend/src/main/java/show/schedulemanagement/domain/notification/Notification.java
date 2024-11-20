7567b14644ddbae816c78f4ad139a69529ab8504
siho-song <thdtlgh234@naver.com>
2024-11-20 19:03:56 +0900

    [Feat] 엔티티 구조 변경 - 필드 추가

--- 8to10_backend/src/main/java/show/schedulemanagement/domain/notification/Notification.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/domain/notification/Notification.java
@@ -6,22 +6,21 @@ import static lombok.AccessLevel.*;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
-import show.schedulemanagement.domain.auditing.baseentity.BaseEntity;
 import show.schedulemanagement.domain.auditing.baseentity.BaseTimeEntity;
 import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.service.event.NotificationEvent;
 
 @Entity
 @Getter
@@ -44,8 +43,30 @@ public class Notification extends BaseTimeEntity {
     @ColumnDefault(value = "false")
     private boolean isRead;
 
-    private Long entityId;
+    private String targetUrl;
+
+    private Long relatedEntityId;
 
     @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     private NotificationType notificationType;
+
+    public static Notification from(Member member, NotificationEvent event){
+        Notification notification = new Notification();
+        notification.member = member;
+        notification.message = event.getMessage();
+        notification.isRead = false;
+
+        NotificationType notificationType = event.getNotificationType();
+        notification.notificationType = notificationType;
+
+        notification.setTargetUrl(notificationType.getBaseTargetUrl(),event.getTargetEntityId());
+        notification.relatedEntityId = event.getRelatedEntityId();
+
+        return notification;
+    }
+
+    private void setTargetUrl(String baseTargetUrl, Long targetEntityId){
+        this.targetUrl = baseTargetUrl + "/" + targetEntityId;
+    }
 }
