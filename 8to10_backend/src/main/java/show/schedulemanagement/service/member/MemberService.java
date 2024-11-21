655502f32eb2127af0c54cfef1c504918ff37c9f
siho-song <thdtlgh234@naver.com>
2024-11-21 21:02:40 +0900

    [Feat] 모든 멤버 조회 메서드 구현

--- 8to10_backend/src/main/java/show/schedulemanagement/service/member/MemberService.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/service/member/MemberService.java
@@ -1,9 +1,11 @@
 package show.schedulemanagement.service.member;
 
+import java.util.List;
 import show.schedulemanagement.domain.member.Member;
 
 public interface MemberService {
     Member findById(Long id);
     Member findByEmail(String email);
+    List<Member> findAll();
     Member getAuthenticatedMember();
 }
