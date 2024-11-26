ff22377c573e03c3df05d073d1b7586be72228d1
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-26 17:09:14 +0900

    [Feat] TodoList 컴포넌트 구현 (2)
    - events : 커스텀 훅을 통해 풀캘린더 컨텍스트에서 일정을 가져옴
    - 상태 정의
      - currentDate : 투두리스트에서 리렌더링을 유발하는 날짜 변경을 관리하기 위한 상태
      - todoEvents : 투두리스트에 보일 일정들을 관리하기 위한 상태

--- 8to10_frontend/src/components/home/todoList/TodoList.jsx
+++ 8to10_frontend/src/components/home/todoList/TodoList.jsx
@@ -1,7 +1,17 @@
 import TodoListHeader from "@/components/home/todoList/TodoListHeader.jsx";
 import TodoItemsList from "@/components/home/todoList/TodoItemsList.jsx";
+import {useCalendar} from "@/context/fullCalendar/useCalendar.jsx";
+import {useState} from "react";
 
 const TodoList = () => {
+    const {events} = useCalendar();
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const [currentDate, setCurrentDate] = useState(today);
+    const [todoEvents, setTodoEvents] = useState([]);
+    
     return (
         <div className="todo-list-container">
             <h3>TO-DO</h3>
