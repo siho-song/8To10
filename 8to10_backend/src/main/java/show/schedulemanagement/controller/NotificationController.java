36f1cc4f9bfee172bd811997076e10b4aaf372c1
siho-song <thdtlgh234@naver.com>
2024-11-27 03:06:51 +0900

    [Feat] 알림 삭제 엔드포인트 구현

--- 8to10_backend/src/main/java/show/schedulemanagement/controller/NotificationController.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/controller/NotificationController.java
@@ -4,13 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import show.schedulemanagement.config.web.CurrentMember;
 import show.schedulemanagement.domain.member.Member;
+import show.schedulemanagement.service.notification.NotificationService;
 import show.schedulemanagement.service.notification.SseEmitterService;
 
 @RestController
@@ -19,6 +22,13 @@ import show.schedulemanagement.service.notification.SseEmitterService;
 @Slf4j
 public class NotificationController {
     private final SseEmitterService sseEmitterService;
+    private final NotificationService notificationService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@CurrentMember Member member, @PathVariable(value = "id") Long id) {
+        notificationService.deleteById(member, id);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> subscribe(
