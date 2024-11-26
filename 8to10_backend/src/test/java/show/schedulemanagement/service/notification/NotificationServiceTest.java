8e03f9106352be444c7e12f5bfbbe68827c45a15
siho-song <thdtlgh234@naver.com>
2024-11-27 03:19:54 +0900

    [Test] 알림 읽음 상태 업데이트 메서드 테스트 구현

--- 8to10_backend/src/test/java/show/schedulemanagement/service/notification/NotificationServiceTest.java
+++ 8to10_backend/src/test/java/show/schedulemanagement/service/notification/NotificationServiceTest.java
@@ -1,8 +1,9 @@
 package show.schedulemanagement.service.notification;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Optional;
+import jakarta.persistence.EntityNotFoundException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import show.schedulemanagement.repository.notification.NotificationRepository;
 import show.schedulemanagement.service.member.MemberService;
 
 @SpringBootTest
+@DisplayName("알림 서비스 테스트")
 class NotificationServiceTest {
 
     @Autowired
@@ -31,15 +33,33 @@ class NotificationServiceTest {
     @DisplayName("알림 삭제 요청을 날린 유저와 , 알림의 유저가 같으면 알림을 삭제한다.")
     @WithUserDetails("normal@example.com")
     @Transactional
-    void delete(){
+    void delete() {
         //given
         Member member = memberService.getAuthenticatedMember();
         Long notificationId = 1L;
         //when
-        notificationService.deleteById(member,notificationId);
+        notificationService.deleteById(member, notificationId);
 
         //then
         Notification result = notificationRepository.findById(notificationId).orElse(null);
         assertThat(result).isNull();
+    }
+
+    @Test
+    @DisplayName("알림 읽음상태 변경 요청을 날린 유저와, 알림의 유저가 같으면 읽음 상태를 읽음으로 변경한다.")
+    @WithUserDetails("normal@example.com")
+    void update(){
+        //given
+        Member member = memberService.getAuthenticatedMember();
+        Long notificationId = 1L;
+
+        //when
+        notificationService.updateReadStatus(member, notificationId);
+
+        //then
+        Notification result = notificationRepository.findById(notificationId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertThat(result.isRead()).isTrue();
     }
 }
