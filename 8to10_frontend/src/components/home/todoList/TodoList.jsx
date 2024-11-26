bc129438332ef650a3c94c67e8c9c7cd22c2a30b
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-26 17:02:01 +0900

    [Feat] TodoListHeader 컴포넌트 구현 (1)
    TodoList 컴포넌트에서 TodoListHeader 컴포넌트 분리

--- 8to10_frontend/src/components/home/todoList/TodoList.jsx
+++ 8to10_frontend/src/components/home/todoList/TodoList.jsx
@@ -1,13 +1,10 @@
+import TodoListHeader from "@/components/home/todoList/TodoListHeader.jsx";
 
 const TodoList = () => {
     return (
         <div className="todo-container">
             <h3>TO-DO</h3>
-            <div id="date-navigation">
-                <button id="prev-date-btn">이전</button>
-                <span id="current-date"></span>
-                <button id="next-date-btn">다음</button>
-            </div>
+            <TodoListHeader/>
             <div id="todo-list"></div>
             <button id="submit-todo-btn">제출</button>
         </div>
---
+++ 8to10_frontend/src/components/home/todoList/TodoListHeader.jsx
@@ -0,0 +1,12 @@
+
+const TodoListHeader = () => {
+    return (
+        <div id="todo-list-header">
+            <button id="prev-date-btn">이전</button>
+            <span id="current-date"></span>
+            <button id="next-date-btn">다음</button>
+        </div>
+    );
+}
+
+export default TodoListHeader;
