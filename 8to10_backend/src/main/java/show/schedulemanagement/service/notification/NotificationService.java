01acef60e27e7043d15f8285197bda39dd2cda70
siho-song <thdtlgh234@naver.com>
2024-11-27 03:06:09 +0900

    [Feat] 알림 삭제 메서드 구현

--- 8to10_backend/src/main/java/show/schedulemanagement/service/notification/NotificationService.java
+++ 8to10_backend/src/main/java/show/schedulemanagement/service/notification/NotificationService.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -16,7 +17,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import show.schedulemanagement.domain.member.Member;
 import show.schedulemanagement.domain.notification.Notification;
+import show.schedulemanagement.exception.ExceptionCode;
 import show.schedulemanagement.exception.InvalidRedisMessageException;
+import show.schedulemanagement.exception.NotFoundEntityException;
 import show.schedulemanagement.repository.notification.NotificationRepository;
 import show.schedulemanagement.service.event.NotificationEvent;
 
@@ -34,6 +37,15 @@ public class NotificationService {
     @Transactional
     public void save(Notification notification){
         notificationRepository.save(notification);
+    }
+
+    @Transactional
+    public void deleteById(Member member, Long id) {
+        Notification notification = notificationRepository.findByIdWithMember(id)
+                .orElseThrow(() -> new NotFoundEntityException(ExceptionCode.NOT_FOUND_NOTIFICATION));
+        if(member.isSameEmail(notification.getMember().getEmail())){
+            notificationRepository.deleteById(id);
+        }
     }
 
     @Transactional(readOnly = true)
