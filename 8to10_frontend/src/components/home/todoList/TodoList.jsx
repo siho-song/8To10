3f97ef2a15dbf2ab5b587bf06088392616326827
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-26 17:17:45 +0900

    [Feat] TodoList 컴포넌트 구현 (3)
    - filterByEventsByDate : 날짜를 기준으로 풀캘린더의 일반 일정들을 필터링 해주는 함수
    - useEffect 훅 : 풀캘린더의 일정들을 담는 events와 currentDate 상태가 바뀔때마다 투두리스트가 리렌더링 되도록 구현
    - changeCurrentDate : date를 인자로 받아 currentDate를 바꿔주는 함수

--- 8to10_frontend/src/components/home/todoList/TodoList.jsx
+++ 8to10_frontend/src/components/home/todoList/TodoList.jsx
@@ -1,7 +1,7 @@
 import TodoListHeader from "@/components/home/todoList/TodoListHeader.jsx";
 import TodoItemsList from "@/components/home/todoList/TodoItemsList.jsx";
 import {useCalendar} from "@/context/fullCalendar/useCalendar.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const TodoList = () => {
     const {events} = useCalendar();
@@ -11,13 +11,42 @@ const TodoList = () => {
 
     const [currentDate, setCurrentDate] = useState(today);
     const [todoEvents, setTodoEvents] = useState([]);
-    
+
+    const filterEventsByDate = (targetDate) => {
+        if (!events) return [];
+
+        const formattedTargetDate = new Date(targetDate).toDateString();
+
+        return events.filter((event) => {
+            const eventStartDate = new Date(event.start).toDateString();
+            return eventStartDate === formattedTargetDate && event.extendedProps.type === "normal";
+        });
+    };
+
+    useEffect(() => {
+        setTodoEvents(filterEventsByDate(events));
+    }, [events, currentDate]);
+
+    const changeCurrentDate = (date) => {
+        const newDate = new Date(date);
+        setCurrentDate(newDate);
+    }
+
+    const handleTodoSubmit = (e) => {
+        console.log("todolist 제출")
+    }
+
     return (
         <div className="todo-list-container">
             <h3>TO-DO</h3>
             <TodoListHeader/>
             <TodoItemsList/>
-            <button id="submit-todo-btn">제출</button>
+            <button
+                id="submit-todo-btn"
+                onClick={handleTodoSubmit}
+            >
+                제출
+            </button>
         </div>
     );
 }
