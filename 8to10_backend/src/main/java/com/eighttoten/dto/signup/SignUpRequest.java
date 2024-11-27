8a1a4d6d0fbd17ccbb7574296ed12f7dd90bcb92
siho-song <thdtlgh234@naver.com>
2024-11-28 00:47:44 +0900

    [Style] 공백 제거

--- 8to10_backend/src/main/java/com/eighttoten/dto/signup/SignUpRequest.java
+++ 8to10_backend/src/main/java/com/eighttoten/dto/signup/SignUpRequest.java
@@ -1,16 +1,16 @@
 package com.eighttoten.dto.signup;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 import com.eighttoten.domain.member.Gender;
 import com.eighttoten.domain.member.Mode;
 import com.eighttoten.validator.signup.fielderror.Nickname;
 import com.eighttoten.validator.signup.fielderror.Password;
 import com.eighttoten.validator.signup.fielderror.PhoneNumber;
 import com.eighttoten.validator.signup.fielderror.Username;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
