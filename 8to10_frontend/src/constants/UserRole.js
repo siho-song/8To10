fcf55a4d3c5a5d6dd1d89ad57d43e1ecb54e8f7a
Woosang An <dntkd857@soongsil.ac.kr>
2024-11-28 19:55:22 +0900

    [Feat] USER_ROLE 상수 및 사용자 기본 이미지 경로 상수로 저장

---
+++ 8to10_frontend/src/constants/UserRole.js
@@ -0,0 +1,11 @@
+export const USER_ROLE = {
+    ADMIN: "관리자",
+    NORMAL_USER: "일반 유저",
+    FAITHFUL_USER: "성실 유저",
+}
+
+export const DEFAULT_PROFILE_IMAGE_PATH = {
+    ADMIN: "/src/assets/images/normalUser.svg",
+    NORMAL_USER: "/src/assets/images/normalUser.svg",
+    FAITHFUL_USER: "/src/assets/images/faithfulUser.svg",
+}
