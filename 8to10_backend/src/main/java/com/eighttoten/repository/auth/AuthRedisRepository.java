ddbba8f65cb7c5685f648cfd91f40b7eae7a435f
siho-song <thdtlgh234@naver.com>
2024-12-02 19:45:09 +0900

    [Feat] 레디스 리포지토리로 변경

--- 8to10_backend/src/main/java/com/eighttoten/repository/auth/AuthRedisRepository.java
+++ 8to10_backend/src/main/java/com/eighttoten/repository/auth/AuthRedisRepository.java
@@ -2,8 +2,8 @@ package com.eighttoten.repository.auth;
 
 import com.eighttoten.domain.auth.Auth;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface AuthRepository extends JpaRepository<Auth, Long> {
+public interface AuthRedisRepository extends CrudRepository<Auth, String> {
     Optional<Auth> findByRefreshToken(String refreshToken);
 }
