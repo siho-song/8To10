f82764bacfbaded20011336e97ae7657c81897b8
siho-song <thdtlgh234@naver.com>
2024-12-02 19:46:45 +0900

    [Feat] 함수 실행시간 로깅 Aspect 구현

---
+++ 8to10_backend/src/main/java/com/eighttoten/aspect/ExecutionTimeAspect.java
@@ -0,0 +1,28 @@
+package com.eighttoten.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ExecutionTimeAspect {
+    @Pointcut("execution(* com.eighttoten.service..*(..)) ||"
+            + "execution(* com.eighttoten.controller..*(..)) ||"
+            + "execution(* com.eighttoten.repository..*(..)))")
+    public void businessLogicPointcut(){}
+
+    @Around("businessLogicPointcut()")
+    public Object loggingMethodExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+        log.info("Method name : {} , executed in {} ms", joinPoint.getSignature(), executionTime);
+        return result;
+    }
+}
