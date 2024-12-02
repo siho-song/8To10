634c0015afc4f297ccc06e699dfd218343ccfbad
siho-song <thdtlgh234@naver.com>
2024-12-02 19:47:04 +0900

    [Feat] CGLIB 프록시 적용

--- 8to10_backend/src/main/java/com/eighttoten/EightToTenApplication.java
+++ 8to10_backend/src/main/java/com/eighttoten/EightToTenApplication.java
@@ -2,12 +2,14 @@ package com.eighttoten;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class EightToTenApplication {
     public static void main(String[] args) {
         SpringApplication.run(EightToTenApplication.class, args);
