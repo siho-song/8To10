768c8e0e1e0320a6687e096eaa4d58b35c39bc42
Woosang An <dntkd857@soongsil.ac.kr>
2024-12-04 00:08:05 +0900

    [Feat] 회원가입 컴포넌트 구현 (4)
    - 회원 가입 완료 컴포넌트 구현
    - App.jsx에 라우트 추가

---
+++ 8to10_frontend/src/components/signUp/SignUpComplete.jsx
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import PublicHeader from "@/components/PublicHeader.jsx";
+import "@/styles/signUp/SignUpComplete.css"
+
+const SignUpComplete = () => {
+    return (
+        <>
+            <PublicHeader />
+            <div className="completion-container">
+                <h2>회원가입 완료!</h2>
+                <p>회원가입이 성공적으로 완료되었습니다. 이제 일정 관리 애플리케이션 EightToTen의 모든 기능을 자유롭게 이용할 수 있습니다.</p>
+                <div className="button-group">
+                    <Link to="/" className="button">로그인하기</Link>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default SignUpComplete;
