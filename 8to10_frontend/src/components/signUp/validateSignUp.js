4354bed06bd72b33cd8473352b05de849e0a0c56
Woosang An <dntkd857@soongsil.ac.kr>
2024-12-04 00:07:27 +0900

    [Feat] 회원가입 컴포넌트 구현 (3)
    - 검증 함수 구현
    - 검증 메시지, 정규식을 담을 상수 파일 생성

--- 8to10_frontend/src/components/signUp/validateSignUp.js
+++ 8to10_frontend/src/components/signUp/validateSignUp.js
@@ -1,42 +1,47 @@
 import authenticatedApi from "@/api/AuthenticatedApi.js";
 import {API_ENDPOINT_NAMES} from "@/constants/ApiEndPoints.js";
+import {
+    EMAIL_FIELD_MESSAGE,
+    EMAIL_PATTERN,
+    NAME_FIELD_MESSAGE,
+    NAME_PATTERN, NICKNAME_FIELD_MESSAGE,
+    NICKNAME_PATTERN, PASSWORD_FIELD_MESSAGE, PASSWORD_PATTERN, PHONE_NUMBER_FIELD_MESSAGE,
+    PHONE_NUMBER_PATTERN
+} from "@/constants/SignUpValidator.js";
 
 export const validateName = (name) => {
-    const NAME_PATTERN = /^[가-힣]{2,6}$/;
 
     if (!name || name.trim() === "") {
-        return "이름을 입력해주세요.";
+        return NAME_FIELD_MESSAGE.EMPTY;
     }
     if (!NAME_PATTERN.test(name)) {
-        return "이름은 한글 2~6글자여야 합니다.";
+        return NAME_FIELD_MESSAGE.INVALID_VALUE;
     }
     return null;
 };
 
 export const validateNickname = async (nickname) => {
-    const NICKNAME_PATTERN = /^[가-힣a-zA-Z0-9_.]+$/;
-
     if (!nickname || nickname.trim() === "") {
-        return "닉네임을 입력해주세요.";
+        return NICKNAME_FIELD_MESSAGE.EMPTY;
     }
 
     if (!NICKNAME_PATTERN.test(nickname)) {
-        return "닉네임은 한글, 영문, 숫자, '_', '.' 만 사용할 수 있습니다.";
+        return NICKNAME_FIELD_MESSAGE.INVALID_VALUE;
     }
 
     if (/.*[가-힣]+.*/.test(nickname)) {
         if (nickname.length < 2 || nickname.length > 12) {
-            return "한글 닉네임은 2~12자 이내여야 합니다.";
+            return NICKNAME_FIELD_MESSAGE.KOREAN_LENGTH_CONSTRAINT;
         }
     } else {
         if (nickname.length > 20) {
-            return "영문 닉네임은 20자 이내여야 합니다.";
+            return NICKNAME_FIELD_MESSAGE.LENGTH_CONSTRAINT;
         }
     }
 
     const isDuplicated = await isDuplicatedNickname(nickname);
     if (isDuplicated) {
-        return "사용중인 닉네임 입니다. 다른 닉네임을 입력해주세요."
+        return NICKNAME_FIELD_MESSAGE.DUPLICATED;
     }
     return null;
 };
@@ -57,26 +62,25 @@ export const isDuplicatedNickname = async (nickname) => {
 }
 
 export const validateEmail = async (emailId, emailDomain, customDomain = "") => {
-    const domainRegex = /^(?!-)[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.[A-Za-z]{2,63}$/;
     let email = emailId + '@' + (emailDomain === 'custom' ? customDomain : emailDomain);
 
     if (!emailId) {
-        return "아이디를 입력해 주세요.";
+        return EMAIL_FIELD_MESSAGE.EMAIL_ID_EMPTY;
     }
 
     if (!emailDomain || (emailDomain === "custom" && !customDomain)) {
-        return "이메일 도메인을 입력해주세요.";
+        return EMAIL_FIELD_MESSAGE.EMAIL_DOMAIN_EMPTY;
     }
 
     if (customDomain) {
-        if (!domainRegex.test(customDomain)) {
-            return "올바른 이메일 도메인을 입력해 주세요. 예: example.com";
+        if (!EMAIL_PATTERN.test(customDomain)) {
+            return EMAIL_FIELD_MESSAGE.EMAIL_INVALID_DOMAIN;
         }
     }
 
     const isDuplicated = await isDuplicatedEmail(email);
     if (isDuplicated) {
-        return "사용 중인 이메일입니다. 다른 이메일을 입력해주세요";
+        return EMAIL_FIELD_MESSAGE.DUPLICATED;
     }
 
     return "";
@@ -98,43 +102,40 @@ const isDuplicatedEmail = async (email) => {
 }
 
 export function validatePhone(phone) {
-    const phoneRegex = /^(01[0-9])\d{7,8}$/;
     if (!phone) {
-        return "전화번호를 입력해 주세요.";
+        return PHONE_NUMBER_FIELD_MESSAGE.EMPTY;
     }
-    if (!phoneRegex.test(phone)) {
-        return "전화번호는 010~019로 시작하고, 10~11자리 숫자여야 합니다.";
+    if (!PHONE_NUMBER_PATTERN.test(phone)) {
+        return PHONE_NUMBER_FIELD_MESSAGE.INVALID_VALUE;
     }
 
     return "";
 }
 
 export function validatePassword(password) {
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$/;
 
     if (!password) {
-        return "비밀번호를 입력해 주세요.";
+        return PASSWORD_FIELD_MESSAGE.EMPTY;
     }
 
-    if (!passwordRegex.test(password)) {
-        return "비밀번호는 8~20자 사이여야 하며, 영문, 숫자, 특수문자(!@#$%^&*?_)를 포함해야 합니다.";
+    if (!PASSWORD_PATTERN.test(password)) {
+        return PASSWORD_FIELD_MESSAGE.INVALID_VALUE;
     }
     return "";
 }
 
 export function validateConfirmationPassword(password, confirmationPassword) {
-    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,20}$/;
 
-    if (!passwordRegex.test(confirmationPassword)) {
-        return "비밀번호는 8~20자 사이여야 하며, 영문, 숫자, 특수문자(!@#$%^&*?_)를 포함해야 합니다.";
+    if (!PASSWORD_PATTERN.test(confirmationPassword)) {
+        return PASSWORD_FIELD_MESSAGE.INVALID_VALUE;
     }
 
     if (!confirmationPassword) {
-        return "비밀번호 확인을 입력해 주세요.";
+        return PASSWORD_FIELD_MESSAGE.EMPTY_CONFIRMATION_PASSWORD;
     }
 
     if (password !== confirmationPassword) {
-        return "비밀번호와 비밀번호 확인이 일치하지 않습니다.";
+        return PASSWORD_FIELD_MESSAGE.CONFIRMATION_PASSWORD_MISMATCH;
     }
 
     return "";
