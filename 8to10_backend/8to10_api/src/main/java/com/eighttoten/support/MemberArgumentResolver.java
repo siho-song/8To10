a0709253394a226c86db39ad0ac7cfa79057c13e
siho-song <thdtlgh234@naver.com>
2025-02-04 13:22:48 +0900

    [Feat] 시큐리티 의존성 제거

--- 8to10_backend/8to10_api/src/main/java/com/eighttoten/support/MemberArgumentResolver.java
+++ 8to10_backend/8to10_api/src/main/java/com/eighttoten/support/MemberArgumentResolver.java
@@ -1,18 +1,19 @@
-package com.eighttoten.global;
+package com.eighttoten.support;
 
-import com.eighttoten.infrastructure.security.domain.MemberDetails;
 import com.eighttoten.member.domain.Member;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
 import org.springframework.core.MethodParameter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
 public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final AuthAccessor authAccessor;
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(CurrentMember.class) &&
@@ -24,19 +25,13 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
             MethodParameter parameter,
             ModelAndViewContainer mavContainer,
             NativeWebRequest webRequest,
-            WebDataBinderFactory binderFactory) throws Exception
+            WebDataBinderFactory binderFactory)
     {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
         if (request == null) {
             return new BadRequestException("잘못된 요청 입니다.");
         }
 
-        return getAuthenticatedMember();
-    }
-
-    public Member getAuthenticatedMember() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        MemberDetails principal = (MemberDetails) authentication.getPrincipal();
-        return principal.getMember();
+        return authAccessor.getAuthenticatedMember();
     }
 }
