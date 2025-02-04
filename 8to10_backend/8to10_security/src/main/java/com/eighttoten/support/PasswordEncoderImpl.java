7028bc9eac78ed061cffb5e76bc0193932ae4816
siho-song <thdtlgh234@naver.com>
2025-02-04 12:49:56 +0900

    [Feat] PasswordEncoder 인터페이스 추가

---
+++ 8to10_backend/8to10_security/src/main/java/com/eighttoten/support/PasswordEncoderImpl.java
@@ -0,0 +1,14 @@
+package com.eighttoten.support;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+public class PasswordEncoderImpl implements PasswordEncoder {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public String encode(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
+}
