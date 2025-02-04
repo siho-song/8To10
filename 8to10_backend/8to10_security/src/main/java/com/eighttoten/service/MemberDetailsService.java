2ffd39d3468ff262bf32b1c36bff3c4c63a260fe
siho-song <thdtlgh234@naver.com>
2025-02-04 12:47:29 +0900

    [Feat] 예외명 변경 및 인증된 회원조회 메서드 추가

---
+++ 8to10_backend/8to10_security/src/main/java/com/eighttoten/service/MemberDetailsService.java
@@ -0,0 +1,34 @@
+package com.eighttoten.service;
+
+import com.eighttoten.exception.ExceptionCode;
+import com.eighttoten.domain.MemberDetails;
+import com.eighttoten.exception.CustomAuthenticationException;
+import com.eighttoten.member.domain.Member;
+import com.eighttoten.member.domain.MemberRepository;
+import com.eighttoten.support.AuthAccessor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomAuthenticationException(ExceptionCode.INVALID_EMAIL));
+        return new MemberDetails(member);
+    }
+
+    public Member getAuthenticatedMember() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        MemberDetails principal = (MemberDetails) authentication.getPrincipal();
+        return principal.getMember();
+    }
+}
