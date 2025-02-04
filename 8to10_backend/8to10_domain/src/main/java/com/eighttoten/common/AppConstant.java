25496195d7943590f0897da6fcdc3868f2fe7cd6
siho-song <thdtlgh234@naver.com>
2025-02-04 13:08:50 +0900

    [Feat] 상수클래스 정의

--- 8to10_backend/8to10_domain/src/main/java/com/eighttoten/common/AppConstant.java
+++ 8to10_backend/8to10_domain/src/main/java/com/eighttoten/common/AppConstant.java
@@ -1,14 +1,19 @@
-package com.eighttoten.global.constant;
+package com.eighttoten.common;
 
 import jakarta.annotation.PostConstruct;
+import java.time.LocalTime;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@NoArgsConstructor
-public class AppConstantValidator {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class AppConstant {
+    public static final LocalTime WORK_START_TIME = LocalTime.of(8, 0);
+    public static final LocalTime WORK_END_TIME = LocalTime.of(22, 0);
+
     @PostConstruct
-    public void validateAppConstant(){
+    void init(){
         if(AppConstant.WORK_START_TIME.isAfter(AppConstant.WORK_END_TIME)){
             throw new RuntimeException("WORK_START_TIME 은 WORK_END_TIME 보다 이전이어야 합니다.");
         }
